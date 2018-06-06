0012f2ed585cbb4061bed97b33925d523988d889
paarmita <paarmita1998@gmail.com>
2018-06-06 18:52:23 +0530

    introduction section added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Navigation from './components/Navigation/Navigation';
 import Footer from './components/Footer/Footer';
 import Contributors from './components/Contributors/Contributors';
 import GetStarted from './components/Get_started/Get_started';
+import Introduction from './components/Introduction/Introduction';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navigation />
-        <div className="box"></div>
+        <Introduction/>
         <Contributors />
         <GetStarted />
         <Footer />
