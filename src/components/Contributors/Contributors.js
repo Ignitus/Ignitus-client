477c8b2b748f4447213ae178799ac3718af63f2b
paarmita <paarmita1998@gmail.com>
2018-06-06 14:13:17 +0530

    contributors images

--- src/components/Contributors/Contributors.js
+++ src/components/Contributors/Contributors.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
-import logo from '../../images/black_logo.png';
+import './contributors.css';
+import logo from '../../images/Stanford_logo.png';
 
 class Contributors extends Component {
   render() {
     return (
-      <div >Paarmita</div>
+      <div>Contributors
+        <div>Made with <img src={logo} width="10" height="10" alt={"logo"}/> by Students,Researches and Alumni of ...
+          <div class="row">
+              <div class="column">
+                <img src={logo} width="50" height="50" alt={"logo"}/> 
+              </div>
+              <div class="column">
+               <img src={logo} width="50" height="50" alt={"logo"}/> 
+              </div>
+              <div class="column">
+                <img src={logo} width="50" height="50" alt={"logo"}/> 
+              </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
