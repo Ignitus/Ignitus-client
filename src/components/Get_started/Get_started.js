7cc58fe4ed1931085555f25d3007433e045e322e
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-06-09 17:43:45 +0530

    heroku fix one

--- src/components/Get_started/Get_started.js
+++ src/components/Get_started/Get_started.js
@@ -10,7 +10,7 @@ class GetStarted extends Component {
         <div className = "_align container">
               <div className = "_started" >Get started for free - join thousands of students and researchers 
               already using Ignitus to share their knowledge, work together, and get amazing opportunites.</div>
-              <a href="http://bit.do/join-ignitus" className="btn btn-md _button" role="button">Sign Up</a>
+              <a href="#" className="btn btn-md _button" role="button">Sign Up</a>
          </div>
         
     )
