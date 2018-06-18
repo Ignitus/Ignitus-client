ffc82117c347a276681fb5460ff6553931317913
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-06-18 19:03:36 +0530

    partners and supporters logos added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navigation from './components/Navigation/Navigation';
-import Footer from './components/Footer/Footer';
+import Navigation   from './components/Navigation/Navigation';
+import Footer       from './components/Footer/Footer';
 import Contributors from './components/Contributors/Contributors';
-import GetStarted from './components/Get_started/Get_started';
+import GetStarted   from './components/Get_started/Get_started';
 import Introduction from './components/Introduction/Introduction';
-import CardLayout from './components/CardLayout/CardLayout';
+import CardLayout   from './components/CardLayout/CardLayout';
+import Partner      from './components/Partners/Partners';
 
 class App extends Component {
   render() {
@@ -16,7 +17,9 @@ class App extends Component {
         <CardLayout />
         <Contributors />
         <GetStarted />
+        <Partner />
         <Footer />
+        
       </div>
     );
   }
