1c5636074a61ed000604e91628769f05cb33179b
afreen23 <studentinsha12@gmail.com>
2018-07-05 20:20:44 +0530

    tried fixing testimonials responsiveness

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -24,15 +24,15 @@ class CarouselSlide extends Component {
       <li
         className={
           this.props.index == this.props.activeIndex
-            ? "carousel__slide carousel__slide--active"
+            ? "carousel__slide carousel__slide--active "
             : "carousel__slide"
         }>
-        <div className="carousel-slide__content">{this.props.slide.content}</div>
+        <div className="carousel-slide__content"  >{this.props.slide.content}</div>
 
-        <div>
+        <div className = "author-source-container">
           <strong className="carousel-slide__author">
             {this.props.slide.author}
-          </strong>,
+          </strong>
           {" "}
           <small className="carousel-slide__source">
             {this.props.slide.source}
@@ -47,7 +47,7 @@ class CarouselLeftArrow extends Component {
   render() {
     return (
       <a
-        href="#"
+        href="#"     
         className="carousel__arrow carousel__arrow--left"
         onClick={this.props.onClick}>
         <i className="fa fa-2x fa-angle-left"> </i>
@@ -61,6 +61,7 @@ class CarouselRightArrow extends Component {
     return (
       <a
         href="#"
+         
         className="carousel__arrow carousel__arrow--right"
         onClick={this.props.onClick}>
         <i className="fa fa-2x fa-angle-right"> </i>
@@ -72,7 +73,7 @@ class CarouselRightArrow extends Component {
 // Carousel wrapper component
 class Testimonial extends Component {
 
-	constructor(props) {
+  constructor(props) {
     super(props);
 
     this.goToSlide     = this.goToSlide.bind(this);
@@ -150,31 +151,34 @@ class Testimonial extends Component {
   render() {
 
     return (
-      <div className="carousel custom_css">
-        <CarouselLeftArrow onClick={e => this.goToPrevSlide(e)} />
-
-        <ul className="carousel__slides">
-          {this.props.slides.map((slide, index) =>
-            <CarouselSlide
-              key={index}
-              index={index}
-              activeIndex={this.state.activeIndex}
-              slide={slide}/>
-          )}
-        </ul>
-
-        <CarouselRightArrow onClick={e => this.goToNextSlide(e)} />
-
-        <ul className="carousel__indicators">
-          {this.props.slides.map((slide, index) =>
-            <CarouselIndicator
-              key={index}
-              index={index}
-              activeIndex={this.state.activeIndex}
-              isActive={this.state.activeIndex==index} 
-              onClick={e => this.goToSlide(index)}/>
-          )}
-        </ul>
+      <div className="carousel">
+        <div>
+          <CarouselLeftArrow onClick={e => this.goToPrevSlide(e)} />
+        </div>
+        <div>
+          <ul className="carousel__slides container" >
+            {this.props.slides.map((slide, index) =>
+              <CarouselSlide
+                key={index}
+                index={index}
+                activeIndex={this.state.activeIndex}
+                slide={slide}/>
+            )}
+          </ul>
+          <ul className="carousel__indicators">
+            {this.props.slides.map((slide, index) =>
+              <CarouselIndicator
+                key={index}
+                index={index}
+                activeIndex={this.state.activeIndex}
+                isActive={this.state.activeIndex==index} 
+                onClick={e => this.goToSlide(index)}/>
+            )}
+          </ul>
+        </div>
+        <div className = "arrow-fix">
+          <CarouselRightArrow onClick={e => this.goToNextSlide(e)} />
+        </div>
       </div>
     );
   }
@@ -182,5 +186,11 @@ class Testimonial extends Component {
 }
 
 export default Testimonial;
+   
+
+     
+    
+
+    
 
 
