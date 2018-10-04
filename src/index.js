f63a57309c6cad8ffdfd671bd2cbacb4a077dbf6
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-10-04 22:51:58 +0530

    Added redux

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ import registerServiceWorker from './registerServiceWorker';
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from './store'
 
 ReactDOM.render((
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
 	), document.getElementById('root'));
 registerServiceWorker();
