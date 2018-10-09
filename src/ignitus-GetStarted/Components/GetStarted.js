515234ae6198c44e4f40fd982aa843bfa76a5c8f
Tucker <trblackw@ncsu.edu>
2018-10-09 11:00:56 -0400

    no need to extend component

--- src/ignitus-GetStarted/Components/GetStarted.js
+++ src/ignitus-GetStarted/Components/GetStarted.js
@@ -1,22 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../Styles/style.css';
 
-
-class GetStarted extends Component {
-  render() {
-    return (
-      <div className="_align container">
-        <div className="_started">
-          Get started for free - join thousands of students and researchers
-          already using Ignitus to share their knowledge, work together, and get
-          amazing opportunites.
-        </div>
-        <a href="/signup" className="btn btn-md _button" role="button">
-          Sign Up
-        </a>
-      </div>
-    );
-  }
-}
+const GetStarted = () => (
+  <div className="_align container">
+    <div className="_started">
+      Get started for free - join thousands of students and researchers already
+      using Ignitus to share their knowledge, work together, and get amazing
+      opportunites.
+    </div>
+    <a href="/signup" className="btn btn-md _button" role="button">
+      Sign Up
+    </a>
+  </div>
+);
 
 export default GetStarted;
