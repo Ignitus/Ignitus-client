2f0b5c8901a7a1fbfc8053ecad0f8ae527ba21bd
Tucker <trblackw@ncsu.edu>
2018-10-09 11:03:53 -0400

    no need to extend component

--- src/ignitus-Partners/Components/Partners.js
+++ src/ignitus-Partners/Components/Partners.js
@@ -8,46 +8,44 @@ import girlScript from '../../Assets/Images/Partners Logos/gslogo.png';
 import leanIn from '../../Assets/Images/Partners Logos/lean_in.png';
 import progate from '../../Assets/Images/Partners Logos/progate.jpg';
 
-class Partner extends Component {
-  render() {
-    const imagesArray = [
-      wooTech,
-      sugarLabs,
-      girlScript,
-      leanIn,
-      systersLogo,
-      progate,
-    ];
+const Partner = () => {
+  const imagesArray = [
+    wooTech,
+    sugarLabs,
+    girlScript,
+    leanIn,
+    systersLogo,
+    progate,
+  ];
 
-    const html = imagesArray.map((logo, key) => (
-      <div
-        className="col-lg-4 col-md-4 col-sm-6 col-12 _logo_css centered-image"
-        key={key}
-      >
-        <img src={logo} width="200" alt="logo" />
-      </div>
-    ));
+  const html = imagesArray.map((logo, key) => (
+    <div
+      className="col-lg-4 col-md-4 col-sm-6 col-12 _logo_css centered-image"
+      key={key}
+    >
+      <img src={logo} width="200" alt="logo" />
+    </div>
+  ));
 
-    return (
-      <div id="partners">
-        <div className="col-sm-12 col-md-12 col-lg-12 col-xs-12 _align mt-4">
-          <div className="_contributor">Supporters & Partners</div>
-          <div className="_students_researchers">
-            Supported with
-            {' '}
-            <i className="fa fa-heart _heart" />
-            {' '}
+  return (
+    <div id="partners">
+      <div className="col-sm-12 col-md-12 col-lg-12 col-xs-12 _align mt-4">
+        <div className="_contributor">Supporters & Partners</div>
+        <div className="_students_researchers">
+          Supported with 
+{' '}
+<i className="fa fa-heart _heart" />
+{' '}
 by following
-            Organizations ...
-          </div>
-        </div>
-
-        <div className="container">
-          <div className="row _align-vertically">{html}</div>
-        </div>
+          Organizations ...
+</div>
       </div>
-    );
-  }
-}
+
+      <div className="container">
+        <div className="row _align-vertically">{html}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Partner;
