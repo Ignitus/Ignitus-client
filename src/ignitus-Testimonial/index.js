627b7e2d9281b6e34d7f90ae81d1e1dbe4efc560
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-10-11 16:42:27 +0530

    added selectors

--- src/ignitus-Testimonial/index.js
+++ src/ignitus-Testimonial/index.js
@@ -4,11 +4,13 @@ import * as constants from './constants';
 import * as containers from './Containers';
 import reducer from './reducers';
 import * as sagas from './sagas';
+import * as selectors from './selectors'
 
 export default {
   actions,
   components,
   containers,
+  selectors,
   constants,
   reducer,
   sagas,
