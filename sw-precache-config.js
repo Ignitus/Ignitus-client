1e66983571bd05c799b1819b9af2e03d30761517
Nirmal Kumar K S <kagosekar@gmail.com>
2018-10-18 19:05:35 +0530

    Update service worker configuration (#123)
    * Update service worker config to handle fetch requests and include HTML
    * Passes audit for PWA checks related to service worker
    * Related to https://github.com/Ignitus/Ignitus-Client-Side-Development/issues/109

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,13 +1,15 @@
 module.exports = {
   staticFileGlobs: [
+    'build/index.html',
     'build/static/css/**.css',
     'build/static/js/**.js'
   ],
   swFilePath: './build/service-worker.js',
   stripPrefix: 'build/',
-  handleFetch: false,
   runtimeCaching: [{
     urlPattern: /this\\.is\\.a\\.regex/,
     handler: 'networkFirst'
-  }]
+  }],
+  navigateFallback: '/index.html',
+  navigateFallbackWhitelist: [/^(?!\/__)/]
 }
