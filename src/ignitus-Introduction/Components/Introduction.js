530ff0b304cede96b35ccec63c3bdaf3237a5546
Apoorv Taneja <purutaneja.com@gmail.com>
2018-11-01 18:34:45 +0530

    Scroll effect added (#157)

--- src/ignitus-Introduction/Components/Introduction.js
+++ src/ignitus-Introduction/Components/Introduction.js
@@ -5,7 +5,7 @@ import { withErrorBoundary } from '../../ignitus-Internals';
 import resume from '../../ignitus-Assets/Images/Resume.png';
 
 const Introduction = () => (
-  <div className="col-xs-12 col-md-12 col-sm-12 col-xs-12 mt-5 box">
+  <div className="col-xs-12 col-md-12 col-sm-12 col-xs-12 mt-5 box" >
     <div className="row">
       <div className="col-sm-8 offset-sm-2 col-md-5 col-lg-4 offset-lg-1 offset-md-1">
         <img className="img-fluid" src={resume} alt="resume" align="left" />
