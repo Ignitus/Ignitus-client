eaa25c26aa68086e3754b584490fcb92f65fb8a6
Tucker <trblackw@ncsu.edu>
2018-11-20 15:36:00 -0500

    added object-fit property to images and gave them a bit of padding. I also added conditional to increase the size of the Lean In logo specifically because it was so small. Though it might not be super scalable, it makes it look a lot better

--- src/ignitus-Partners/Components/Partners.js
+++ src/ignitus-Partners/Components/Partners.js
@@ -4,39 +4,47 @@ import { withErrorBoundary } from '../../ignitus-Internals';
 import * as t from './Constants';
 
 const Partner = () => {
-  const array = [t.WOOTECH, t.SUGARLABS, t.SYSTERS, t.PROGATE, t.LEANIN, t.GIRLSCRIPT];
-  const html = array.map((logo, key) => (
-    <div
-      className="col-lg-4 col-md-4 col-sm-6 col-12"
-      key={key}
-    >
-
-      <div className="card mt-4 shadow contributor-card">
-        <div className="card-img-top logoCss centered-image d-flex align-middle">
-          <img src={logo} alt="logo" />
+  const array = [
+    t.WOOTECH,
+    t.SUGARLABS,
+    t.SYSTERS,
+    t.PROGATE,
+    t.LEANIN,
+    t.GIRLSCRIPT,
+  ];
+  console.log(array);
+  const html = array.map(logo => (
+      <div className="col-lg-4 col-md-4 col-sm-6 col-12" key={logo}>
+          <div className="card mt-4 shadow contributor-card">
+              <div className="card-img-top logoCss centered-image d-flex align-middle">
+                  <img
+                      src={logo}
+                      alt="logo"
+                      className={/lean/g.test(logo) ? 'leanIn' : ''}
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
   ));
 
   return (
-    <div id="partners">
-      <div className="col-sm-12 col-md-12 col-lg-12 col-xs-12 text-center mt-4">
-        <div className="partners">Supporters & Partners</div>
-        <div className="studentsResearchers">
-          Supported with
-          {' '}
-          <i className="fa fa-heart heart" />
-          {' '}
-          by following
-          Organizations ...
-        </div>
-      </div>
+      <div id="partners">
+          <div className="col-sm-12 col-md-12 col-lg-12 col-xs-12 text-center mt-4">
+              <div className="partners">Supporters & Partners</div>
+              <div className="studentsResearchers">
+                    Supported with 
+{' '}
+<i className="fa fa-heart heart" />
+{' '}
+by
+                    following Organizations ...
+</div>
+            </div>
 
-      <div className="container">
-        <div className="row align-items-center card_row">{html}</div>
-      </div>
-    </div>
+          <div className="container">
+              <div className="row align-items-center card_row">{html}</div>
+            </div>
+        </div>
   );
 };
 
