cebe0108205005d4c5b32b8dc162a955e102a5af
Trip1701 <bj2336@email.vccs.edu>
2018-12-24 09:31:59 -0500

    Introduction UI Skeleton Implemented

--- src/ignitus-Introduction/Components/Introduction.js
+++ src/ignitus-Introduction/Components/Introduction.js
@@ -1,25 +1,34 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import '../Styles/style.scss';
 import { withErrorBoundary } from '../../ignitus-Internals';
 
-import resume from '../../ignitus-Assets/Images/Resume.png';
+import resume from '../../ignitus-Assets/Images/resume.png';
 
 const Introduction = () => (
   <div className="col-xs-12 col-md-12 col-sm-12 col-xs-12 mt-5 box" >
-    <div className="row">
-      <div className="col-sm-8 offset-sm-2 col-md-5 col-lg-4 offset-lg-1 offset-md-1">
-        <img className="img-fluid" src={resume} alt="resume" align="left" />
-      </div>
-      <div className="col-sm-12 col-md-5 col-lg-7">
-        <div className="intro text-center">Introducing</div>
-        <div className="ignitus text-center">Ignitus</div>
-        <div className="tagline text-center">Ignite Your resume</div>
-        <div className="content text-center">
-          ‘Skyrocketing Your Career By Providing You The Best Global
-          Opportunities.’
+      <div className="ignitus-bg"></div>
+      
+      <div className="intro text-center">Welcome to Ignitus</div>
+
+      <div className="intro__content">
+        <div className="intro__content--upper">
+          Get handpicked top-quality global research <br />
+          and industrial internships, for <span className="bold-txt">free</span>!
         </div>
+
+        <div className="intro__content--lower">
+          <p>Ingnite your resume and get the <span className="bold-txt">best global <br /> opportunities</span>
+              and skyrocekt your career</p>
+              <Link to="/Signup">
+                <button className="btn">
+                  Sign Up
+                </button>
+              </Link>
+        </div>
+        <img className="img-fluid" src={resume} alt="resume" />
       </div>
-    </div>
+
   </div>
 );
 export default withErrorBoundary(Introduction);
