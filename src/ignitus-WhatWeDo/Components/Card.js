77f597ffbaead871e6c535a03d40ac1275a6d5c4
karagudisuma <ksumaudupa@gmail.com>
2019-02-11 19:35:06 +0530

    Implement What we provide? UI  #226

--- src/ignitus-WhatWeDo/Components/Card.js
+++ src/ignitus-WhatWeDo/Components/Card.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { string } from 'prop-types';
 
 const Card = ({ image, title, description }) => (
-  <div className="col-lg-3 mx-auto col-sm-5 mb-5 col-8">
-  <div className = "card mt-4 shadow"> 
-    <div className="flatten-text">
-      <img className="mt-2" alt="card" src={image} />
+  <div className="col-lg-4 col-md-6 mx-auto col-sm-8 mb-5 col-8 col-12">
+    <div className="card mt-4 shadow h-100">
+      <div className="centered-image d-flex">
+        <img className="m-2" alt="card" src={image} />
+      </div>
+      <h5 className="card-title text-center text-dark font-weight-bold mt-3">
+        {title}
+      </h5>
+      <p className="card-text text-center p-4">{description}</p>
     </div>
-    <h5 className="card-title text-center text-dark font-weight-bold mt-3">
-      {title}
-    </h5>
-    <p className="card-text text-center p-4">{description}</p>
-  </div>
   </div>
 );
 
