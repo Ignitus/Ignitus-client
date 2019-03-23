7b6fe10eb1560cc99db61941ba7c975fa4a80463
karagudisuma <ksumaudupa@gmail.com>
2019-03-23 15:07:36 +0530

    Corrections for chat list - #195

---
+++ src/ignitus-Dashboard-Chatlist/Components/Constants.js
@@ -0,0 +1,28 @@
+const chatUsers = [
+  {
+    avatar: 'https://storage.googleapis.com/ignitus/Team/avatar-eugene.png',
+    name: 'Eugene Kunkel',
+  },
+  {
+    avatar: 'https://storage.googleapis.com/ignitus/Team/avatar-george.png',
+    name: 'George Hopper',
+  },
+  {
+    avatar: 'https://storage.googleapis.com/ignitus/Team/avatar-grant.png',
+    name: 'Grant Tucker',
+  },
+  {
+    avatar: 'https://storage.googleapis.com/ignitus/Team/avatar-lauren.png',
+    name: 'Lauren Knight',
+  },
+  {
+    avatar: 'https://storage.googleapis.com/ignitus/Team/avatar-melanie.png',
+    name: 'Melanie Hopper',
+  },
+  {
+    avatar: 'https://storage.googleapis.com/ignitus/Team/avatar-eugene.png',
+    name: 'Eugene Tucker',
+  },
+];
+
+export default chatUsers;
--- src/ignitus-Dashboard-Chatlist/Components/dashBoardChatlist.js
+++ src/ignitus-Dashboard-Chatlist/Components/dashBoardChatlist.js
@@ -1,18 +1,91 @@
-import React from "react";
-import { withErrorBoundary } from "../../ignitus-Internals";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withErrorBoundary } from '../../ignitus-Internals';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import ChatUsers from './Constants';
 
-class dashBoardChatlist extends React.Component {
-  constructor(props) {
-    super(props);
-    
-  }
+const styles = () => ({
+  card: {
+    position: 'fixed',
+    width: '273px',
+    maxHeight: '344px',
+    bottom: '0px',
+    background: '#FFFFFF',
+    boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.1)',
+    borderRadius: '16px',
+    overflow: 'auto',
+  },
+  chatListHeader: {
+    backgroundColor: '#000066',
+    height: '48px',
+    borderRadius: '16px 16px 0px 0px',
+  },
+  chatListTitle: {
+    color: '#FFFFFF',
+    fontFamily: 'Raleway',
+    fontWeight: 'bold',
+    fontSize: '18px',
+  },
+  bigAvatar: {
+    margin: 10,
+    width: 60,
+    height: 60,
+  },
+  listContainer: {
+    overflowY: 'scroll',
+  },
+  listItemContainer: {
+    padding: '0px',
+  },
+  onlineContainer: {
+    width: '10px',
+    height: '10px',
+    backgroundColor: 'green',
+    borderRadius: '50%',
+  },
+})
 
-  render() {
-    
-    return (
-      <div className="mb-0"><p>Chat list</p></div>
-    );
-  }
-}
+const dashBoardChatlist = (props) => {
+  const { classes } = props;
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        title="Chat List"
+        classes={{
+          title: classes.chatListTitle,
+          root: classes.chatListHeader,
+        }}
+      />
+      <CardContent>
+        <List className={classes.listContainer}>
+          {ChatUsers.map((user) => {
+            return (
+              <ListItem className={classes.listItemContainer}>
+                <Avatar
+                  alt={user.name}
+                  src={user.avatar}
+                  className={classes.bigAvatar}
+                />
+                <ListItemText primary={user.name} />
+                <div className={classes.onlineContainer} />
+              </ListItem>
+            )
+          })}
+        </List>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default withErrorBoundary(dashBoardChatlist);
+dashBoardChatlist.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withErrorBoundary(withStyles(styles)(dashBoardChatlist));
