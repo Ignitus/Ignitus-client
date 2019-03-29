59b28cb7830fcddcc3a0d35bddbd9e141d1b916f
karagudisuma <ksumaudupa@gmail.com>
2019-03-29 08:21:27 +0530

    Header changes for log in - partial - #195

--- src/ignitus-Dashboard-Header/Components/dashBoardHeader.js
+++ src/ignitus-Dashboard-Header/Components/dashBoardHeader.js
@@ -1,17 +1,99 @@
 import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Link, Redirect, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { HashLink } from 'react-router-hash-link';
 import logo from '../../ignitus-Assets/Images/nav-logo.svg';
 import { withErrorBoundary } from '../../ignitus-Internals';
+import _ from 'lodash';
+
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
+
+const styles = theme => ({
+  root: {
+    width: '100%'
+  },
+  appbarContainer: {
+    color: '#000066',
+    backgroundColor: 'transparent',
+    boxShadow: 'none',
+    display: 'flex'
+  },
+  title: {
+    fontFamily: 'Raleway',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: '16px',
+    textAlign: 'center',
+    color: '#000066',
+    paddingLeft: '10px',
+    paddingRight: '10px',
+  },
+  
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    background: '#FFFFFF',
+    border: '1px solid #000066',
+    boxSizing: 'border-box',
+    borderRadius: '9px',
+    transition: theme.transitions.create('width'),
+    width: '209px',
+    height: '30px',
+    [theme.breakpoints.up('md')]: {
+      width: 200,
+    }
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+});
 
 class dashBoardHeader extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       redirect: false,
+
+      anchorEl: null,
+      mobileMoreAnchorEl: null,
     };
     this.logout = this.logout.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('this', this);
   }
 
   logout() {
@@ -20,80 +102,108 @@ class dashBoardHeader extends React.Component {
     this.setState({ redirect: true });
   }
 
+
+  handleProfileMenuOpen = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleMenuClose = () => {
+    this.setState({ anchorEl: null });
+    this.handleMobileMenuClose();
+  };
+
+  handleMobileMenuOpen = event => {
+    this.setState({ mobileMoreAnchorEl: event.currentTarget });
+  };
+
+  handleMobileMenuClose = () => {
+    this.setState({ mobileMoreAnchorEl: null });
+  };
+
   render() {
     if (this.state.redirect) {
-      return <Redirect to="/" />;
+        return <Redirect to="/" />
     }
 
-    let email = '';
+
+    let email = ''
+    const { classes } = this.props;
+    const { anchorEl, mobileMoreAnchorEl } = this.state;
+
     if (localStorage.getItem('data')) {
-      email = JSON.parse(localStorage.getItem('data')).email;
+        console.log(localStorage.getItem('data'))
+        email = JSON.parse(localStorage.getItem('data')).email
     }
+
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
     return (
-      <nav className="navbar navbar-expand-lg whitenav">
-        <HashLink className="navbar-brand" to="/#">
-          <img src={logo} width="40" height="40" alt="logo" />
-        </HashLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <i className="fa fa-bars" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="nav navbar-nav">
-            <li className="nav-item active">
-              <HashLink smooth to="/#">
-                <span className="nav-link">
-                  Home
-                  {' '}
-                  <span className="sr-only">(current)</span>
-                </span>
-              </HashLink>
-            </li>
-            <li className="nav-item">
-              <HashLink smooth to="/#what-we-do">
-                <span className="nav-link">What we provide?</span>
-              </HashLink>
-            </li>
-            <li className="nav-item">
-              <HashLink smooth to="/#contributors">
-                <span className="nav-link">Contributors</span>
-              </HashLink>
-            </li>
-            <li className="nav-item">
-              <Link to="/aboutus">
-                <span className="nav-link" href="/aboutus">
-                  About
-                </span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#">
-                Welcome
-                {' '}
-                {email}
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#" onClick={this.logout}>
-                Logout
-              </a>
-            </li>
-          </ul>
+
+        <div className={classes.root}>
+            <AppBar className={classes.appbarContainer}>
+                <Toolbar>
+                    
+                    <HashLink className="navbar-brand" to="/#">
+                        <img src={logo} width="40" height="40" alt="logo" />
+                    </HashLink>
+                    <Typography className={classes.title} variant="h6">
+                        Home
+                    </Typography>
+                    <Typography className={classes.title} variant="h6">
+                        Internships
+                    </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.sectionDesktop}>
+                        <div className={classes.search}>
+                            <InputBase
+                                placeholder="Search…"
+                                classes={{
+                                    root: classes.inputRoot,
+                                    input: classes.inputInput,
+                                }}
+                            />
+                        </div>
+                        <IconButton color="inherit">
+                            <Badge badgeContent={4} color="secondary">
+                                <MailIcon />
+                            </Badge>
+                        </IconButton>
+                        <IconButton color="inherit">
+                            <Badge badgeContent={17} color="secondary">
+                                <NotificationsIcon />
+                            </Badge>
+                        </IconButton>
+                        <IconButton
+                            aria-owns={
+                                isMenuOpen ? 'material-appbar' : undefined
+                            }
+                            aria-haspopup="true"
+                            onClick={this.handleProfileMenuOpen}
+                            color="inherit"
+                        >
+                            <AccountCircle />
+                        </IconButton>
+                    </div>
+                    <div className={classes.sectionMobile}>
+                        <IconButton
+                            aria-haspopup="true"
+                            onClick={this.handleMobileMenuOpen}
+                            color="inherit"
+                        >
+                            <MoreIcon />
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
         </div>
-      </nav>
-    );
+    )
   }
 }
+
 
 dashBoardHeader.propTypes = {
   logUserOut: PropTypes.func.isRequired,
 };
 
-export default withErrorBoundary(dashBoardHeader);
+export default withErrorBoundary(withStyles(styles)(dashBoardHeader))
