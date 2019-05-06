8b8d6f024018f8afb0001d119318632405a6c2ec
arc9693 <archana.choudhary.9693@gmail.com>
2019-05-07 05:17:28 +0530

    Filtered contributors by Id
    Fixes #301

--- src/ignitus-Api/index.js
+++ src/ignitus-Api/index.js
@@ -72,8 +72,25 @@ export async function getContributorsData() {
       }]);
 
       const data = Array.from(new Set(resp[0].data.concat(resp[1].data)));
-      await DBHelper.updateDataInDB(t.CONTRIBUTORS_STORE, data);
-      return data;
+
+      const result = [];
+      async function filterdata(){
+      for (let i = 0; i < data.length; i++) {
+        for (let j = i+1; j < data.length; j++) {
+          if(data[i].id===data[j].id)
+            {data[i].contributions+=data[j].contributions;
+            data[j].contributions=0;}
+        }
+      }
+
+      for (let i = 0; i < data.length; i++)
+        if(data[i].contributions)
+          result.push(data[i]);
+      }
+      
+      await filterdata();
+      await DBHelper.updateDataInDB(t.CONTRIBUTORS_STORE,result);
+      return result;
     }
     return await DBHelper.getDataFromDB(t.CONTRIBUTORS_STORE);
   } catch (err) {
