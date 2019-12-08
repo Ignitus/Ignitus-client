371ecff98b76c390525f68ce494962be0f9264c4
Rawat <divyanshu.r46956@gmail.com>
2019-12-08 14:52:12 +0100

    GCP Migration ★.
    React router DOM update and GCP Migration ★.

--- src/ignitus-Dashboard-Chatlist/Components/Constants.js
+++ src/ignitus-Dashboard-Chatlist/Components/Constants.js
@@ -1,32 +1,32 @@
 const chatUsers = [
   {
     id: 1,
-    avatar: 'https://storage.googleapis.com/ignitus/Team/avatar-eugene.png',
+    avatar: 'https://storage.googleapis.com/ignitus_assets/ig-avatars/Team_avatar-eugene.png',
     name: 'Eugene Kunkel',
   },
   {
     id: 2,
-    avatar: 'https://storage.googleapis.com/ignitus/Team/avatar-george.png',
+    avatar: 'https://storage.googleapis.com/ignitus_assets/ig-avatars/Team_avatar-george.png',
     name: 'George Hopper',
   },
   {
     id: 3,
-    avatar: 'https://storage.googleapis.com/ignitus/Team/avatar-grant.png',
+    avatar: 'https://storage.googleapis.com/ignitus_assets/ig-avatars/Team_avatar-grant.png',
     name: 'Grant Tucker',
   },
   {
     id: 4,
-    avatar: 'https://storage.googleapis.com/ignitus/Team/avatar-lauren.png',
+    avatar: 'https://storage.googleapis.com/ignitus_assets/ig-avatars/Team_avatar-lauren.png',
     name: 'Lauren Knight',
   },
   {
     id: 5,
-    avatar: 'https://storage.googleapis.com/ignitus/Team/avatar-melanie.png',
+    avatar: 'https://storage.googleapis.com/ignitus_assets/ig-avatars/Team_avatar-melanie.png',
     name: 'Melanie Hopper',
   },
   {
     id: 6,
-    avatar: 'https://storage.googleapis.com/ignitus/Team/avatar-eugene.png',
+    avatar: 'https://storage.googleapis.com/ignitus_assets/ig-avatars/Team_avatar-eugene.png',
     name: 'Eugene Tucker',
   },
 ];
