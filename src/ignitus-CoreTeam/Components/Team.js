77d84eb5394d5e53564a42d9e219dda08894ac21
Rawat <divyanshu.r46956@gmail.com>
2020-02-24 20:10:46 +0100

    Refactoring from BS4 => Pure CSS.

--- src/ignitus-CoreTeam/Components/Team.js
+++ src/ignitus-CoreTeam/Components/Team.js
@@ -13,48 +13,21 @@ const PureCoreTeam = ({ team }) => (
 );
 
 const CoreTeam = () => {
-  const getSocialMediaIcons = (index) => {
-    const cardItem = document.getElementById(`member-${index}`);
-    cardItem.classList.remove('display-none');
-    cardItem.classList.add('display-flex');
-  };
-
-  const hideSocialMediaIcons = (index) => {
-    const cardItem = document.getElementById(`member-${index}`);
-    cardItem.classList.remove('display-flex');
-    cardItem.classList.add('display-none');
-  };
-
-  const team = Data.map((item, index) => (
+  const team = Data.map(item => (
     <div
       key={item.title}
-      className="col-6 col-sm-4 col-md-3 avatar-wrapper text-center p-3"
+      className="team-card"
     >
       <div
-        className="avatar bg-white p-3"
-        id="team-avatar-card"
-        onMouseOver={() => getSocialMediaIcons(index)}
-        onMouseOut={() => hideSocialMediaIcons(index)}
+        className="avatar"
       >
         <img
-          className="rounded-circle mw-100 w-75"
+          className="rounded-img"
           src={item.img}
           alt={`avatar ${item.name}`}
         />
         <div className="title-name mt-2">{item.title}</div>
         <div className="title-description">{item.description}</div>
-        <div id={`member-${index}`} className="display-none">
-          {item.linkedin && (
-            <a href={item.linkedin} className="team-sm-icon-color">
-              <i className="fa fa-linkedin-square" />
-            </a>
-          )}
-          {item.angellist && (
-            <a href={item.angellist} className="team-sm-icon-color">
-              <i className="fa fa-angellist" />
-            </a>
-          )}
-        </div>
       </div>
     </div>
   ));
