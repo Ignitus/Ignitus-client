009c631cea24958bca4657e73a7b153b1885b036
Rawat <divyanshu.r46956@gmail.com>
2020-02-28 00:36:53 +0100

    Defined return types for reducer.

--- src/ignitus-Team/reducers.ts
+++ src/ignitus-Team/reducers.ts
@@ -1,15 +1,18 @@
 import * as t from './actionTypes';
-import { TeamActionType } from './types';
+import {TeamActionType, TeamReducerReturnType} from './types';
 
-const initialState = { presets: [], isFetching: false };
+const initialState = {presets: [], isFetching: false};
 function setContributorsData(action: TeamActionType) {
-  return Object.assign({}, { presets: action.data }, { isFetching: false });
+  return Object.assign({}, {presets: action.data}, {isFetching: false});
 }
 function getContributorsData() {
-  return Object.assign({}, { isFetching: true }, { presets: [] });
+  return Object.assign({}, {isFetching: true}, {presets: []});
 }
 
-const contributorsReducer = (state = initialState, action: TeamActionType) => {
+const contributorsReducer = (
+  state = initialState,
+  action: TeamActionType,
+): TeamReducerReturnType => {
   switch (action.type) {
     case t.SET_CONTRIBUTORS_DATA:
       return setContributorsData(action);
