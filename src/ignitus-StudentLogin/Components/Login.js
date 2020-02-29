6665a6f63d07b07a9947f6e62fcfa824d4109fb8
Rawat <divyanshu.r46956@gmail.com>
2020-02-29 10:31:30 +0100

    Minor fixes.

--- src/ignitus-StudentLogin/Components/Login.js
+++ src/ignitus-StudentLogin/Components/Login.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-filename-extension */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from 'react';
