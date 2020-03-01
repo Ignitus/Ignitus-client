737715969570b694c95a6fcda03b50ff4699ad65
Aneree <aneemeh@gmail.com>
2020-03-01 22:50:36 +0530

    refactor: Revert the import statement change

--- src/ignitus-Common-Auth-Student-Prof/Components/CommonSignUpUI.tsx
+++ src/ignitus-Common-Auth-Student-Prof/Components/CommonSignUpUI.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {withErrorBoundary} from '../../ignitus-Internals';
-import {studentAuth, professorAuth} from './constants';
+import { Link } from 'react-router-dom';
+import { withErrorBoundary } from '../../ignitus-Internals';
+import * as t from './constants';
 import '../Styles/style.scss';
 
 const CommonSignUpUI = () => (
@@ -16,7 +16,11 @@ const CommonSignUpUI = () => (
           <div className=" col-lg-5 col-md-5 ml-lg-5 ml-md-4 col-sm-5 p-0 common-auth-ui-css">
             <p className="mb-5 text-center common-ui-text">Student</p>
             <Link to="/signup/student" className="">
-              <img alt="Student auth" className="img-fluid" src={studentAuth} />
+              <img
+                alt="Student auth"
+                className="img-fluid"
+                src={t.studentAuth}
+              />
             </Link>
           </div>
           <div className="col-lg-5 col-md-5 ml-lg-4  ml-md-4 col-sm-5 ml-sm-4  p-0 common-auth-ui-css">
@@ -25,7 +29,7 @@ const CommonSignUpUI = () => (
               <img
                 alt="Professor auth"
                 className="img-fluid"
-                src={professorAuth}
+                src={t.professorAuth}
               />
             </Link>
           </div>
