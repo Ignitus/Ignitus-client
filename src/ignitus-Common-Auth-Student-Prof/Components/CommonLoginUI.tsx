4edcbe73ca9cd751cc51d720407147c8a404a3ce
Aneree <aneemeh@gmail.com>
2020-03-01 22:50:36 +0530

    refactor: Revert the import statement change

--- src/ignitus-Common-Auth-Student-Prof/Components/CommonLoginUI.tsx
+++ src/ignitus-Common-Auth-Student-Prof/Components/CommonLoginUI.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { withErrorBoundary } from '../../ignitus-Internals';
-import * as t from './Constants';
+import * as t from './constants';
 import '../Styles/style.scss';
 
 const CommonLoginUI = () => (
@@ -16,7 +16,11 @@ const CommonLoginUI = () => (
           <div className=" col-lg-5 col-md-5 ml-lg-5 ml-md-4 col-sm-5 col p-0 h-50 common-auth-ui-css">
             <p className="mb-5 text-center common-ui-text">Student</p>
             <Link to="/login/student">
-              <img alt="Student auth" className="img-fluid" src={t.studentAuth} />
+              <img
+                alt="Student auth"
+                className="img-fluid"
+                src={t.studentAuth}
+              />
             </Link>
           </div>
           <div className="col-lg-5 col-md-5 ml-lg-4  ml-md-4 col-sm-5 ml-sm-4 col p-0 common-auth-ui-css">
