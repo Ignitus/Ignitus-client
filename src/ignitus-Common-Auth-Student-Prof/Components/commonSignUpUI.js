863daa97bad07a301a9fec115aa420bb1cae2282
Abhishek Jain <abhishekjai35@gmail.com>
2020-03-02 15:45:24 +0530

    Fixed signup page styling for mobile devices

--- src/ignitus-Common-Auth-Student-Prof/Components/commonSignUpUI.js
+++ src/ignitus-Common-Auth-Student-Prof/Components/commonSignUpUI.js
@@ -6,22 +6,22 @@ import '../Styles/style.scss';
 
 const commonSignUpUI = () => (
   <div className="col-lg-12 col-sm-12 col-md-12 col-12 container-bg">
-    <div className="p-5">
+    <div className="p-0 p-sm-5 pt-5">
 
-      <div className="col-lg-7 col-md-8 col-sm-12 col-8 box-container">
+      <div className="col-lg-7 col-md-8 col-sm-12 col-12 mt-5 box-container">
         <div className="login-signup m-top">
           <h3> Sign Up </h3>
           <p> Who are you? </p>
         </div>
-        <div className="row mt-4">
-          <div className=" col-lg-5 col-md-5 ml-lg-5 ml-md-4 col-sm-5 p-0 common-auth-ui-css">
+        <div className="row mt-5">
+          <div className="col-lg-5 col-md-5 ml-lg-5 ml-md-4 col-sm-5 col p-0 h-50 common-auth-ui-css">
             <p className="mb-5 text-center common-ui-text">Student</p>
             <Link to="/signup/student" className="">
               <img alt="Student auth" className="img-fluid" src={t.studentAuth} />
             </Link>
           </div>
 
-          <div className="col-lg-5 col-md-5 ml-lg-4  ml-md-4 col-sm-5 ml-sm-4  p-0 common-auth-ui-css">
+          <div className="col-lg-5 col-md-5 ml-lg-4  ml-md-4 col-sm-5 ml-sm-4 col p-0 common-auth-ui-css">
             <p className="mb-5 text-center common-ui-text">Professor</p>
             <Link to="/signup/professor" className="">
               <img alt="Professor auth" className="img-fluid" src={t.professorAuth} />
