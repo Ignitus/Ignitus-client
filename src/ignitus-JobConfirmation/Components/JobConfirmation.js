ad679d3d77f3fa9afec5708daacb06b1cca961ff
unknown <ashishnagpal2498@gmail.com>
2020-03-06 23:57:24 +0530

    Job description page design completed

--- src/ignitus-JobConfirmation/Components/JobConfirmation.js
+++ src/ignitus-JobConfirmation/Components/JobConfirmation.js
@@ -5,13 +5,14 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import { withErrorBoundary } from '../../ignitus-Internals';
-
 import '../Styles/style.scss'
 class JobConfirmation extends React.Component {
   constructor(props) {
     super(props);
   }
-
+  state = {
+    apply: false
+  }
   render() {
 
     return (
@@ -24,7 +25,7 @@ class JobConfirmation extends React.Component {
                 Product Design
           </h2>
               <div className="job-btn">
-                <button type="button">Apply</button>
+                <button type="button" onClick={()=> this.setState({apply:true})}>Apply</button>
               </div>
             </div>
           </div>
@@ -81,21 +82,67 @@ class JobConfirmation extends React.Component {
                       <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
+                    <p>We can't wait to meet you.</p>
                     </div>
-
+                    <h4 className="note">Please note that all the applications must be filled by form.</h4>
                   </div>
                 </div>
                 <div className="job-joining-details">
                   <div className="job-info">
-                    <h5 className="job-info-heading">
+                    <h4 className="job-info-heading">
                       Job Info
-                    </h5>
+                    </h4>
                     <div>
                       <h5>
                         Location
                   </h5>
                       <p>
                         Pitsburg , New Jersy
+                  </p>
+                    </div>
+                    <div>
+                      <h5>
+                        Contract
+                  </h5>
+                      <p>
+                       Internship
+                  </p>
+                    </div>
+                    <div>
+                      <h5>
+                        Job Category
+                  </h5>
+                      <p>
+                        Designer
+                  </p>
+                    </div>
+                    <div>
+                      <h5>
+                        Experience Level
+                  </h5>
+                      <p>
+                        Beginner, Intermediate, Proficient
+                  </p>
+                    </div>
+                  </div>
+                  <div className="job-contact">
+                  <h4 className="job-info-heading">
+                      Contact
+                    </h4>
+                    <div>
+                      <h5>
+                       Recruiter
+                    </h5>
+                      <p>
+                        Name
+                  </p>
+                    </div>
+                    <div>
+                      <h5>
+                       Email
+                    </h5>
+                      <p>
+                        abc@xyz.com
                   </p>
                     </div>
                   </div>
@@ -105,6 +152,30 @@ class JobConfirmation extends React.Component {
             </div>
           </div>
         </div>
+        {this.state.apply && 
+        <div className="card-outer">
+          <div className="card-body">
+            <div className="card-content">
+              <div className="cancelBtn"><i onClick={()=> this.setState({apply:false})} className="fa fa-times"></i></div>
+              <h3>Thank you for your application!</h3>
+              <p>Its been great to see that you are interest in joining Ignitus.Your application has been received</p>
+              <div className="card-links">
+                <div>
+                <div className="social-link fb">
+                <i className="fab fa-facebook-f"></i>
+                                </div>
+                <div className="social-link twitter">
+                  <i className="fab fa-twitter"></i>
+                </div>
+                <div className="social-link linkedIn">
+                  <i className="fab fa-linkedin"></i>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    }
       </div>
     );
   }
