d80064f0d1740d14f9e2f54eb55ca381741ed978
Tanisha <tanisha031199@gmail.com>
2020-03-10 00:03:11 +0530

    added

--- src/shared/Components/Dashboard/SideNavigation.js
+++ src/shared/Components/Dashboard/SideNavigation.js
@@ -59,6 +59,48 @@ export default function SideNavigation() {
           </svg>
           Saved
         </div>
+        <div class="option liked">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <mask
+              id="mask11"
+              mask-type="alpha"
+              maskUnits="userSpaceOnUse"
+              x="2"
+              y="3"
+              width="20"
+              height="19"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M12 21.35L10.55 20.03C5.4 15.36 2 12.28 2 8.5C2 5.42 4.42 3 7.5 3C9.24 3 10.91 3.81 12 5.09C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.42 22 8.5C22 12.28 18.6 15.36 13.45 20.04L12 21.35Z"
+                fill="white"
+              />
+            </mask>
+            <g mask="url(#mask11)">
+              <rect x="-13" y="-13" width="50" height="50" fill="black" />
+              <mask
+                id="mask12"
+                mask-type="alpha"
+                maskUnits="userSpaceOnUse"
+                x="-13"
+                y="-13"
+                width="50"
+                height="50"
+              >
+                <rect x="-13" y="-13" width="50" height="50" fill="white" />
+              </mask>
+              <g mask="url(#mask12)" />
+            </g>
+          </svg>
+          Liked
+        </div>
         <div class="option applied">
           <svg
             width="25"
