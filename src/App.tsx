0f784dc2f3fa897a2d361fe41e676c1795ba04cd
Rawat <divyanshu.r46956@gmail.com>
2020-03-10 22:19:21 +0100

    Manual Casing fixes  git 🔨

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { withErrorBoundary } from './ignitus-Shared/Components/errorBoundary/index.js';
+import { withErrorBoundary } from './ignitus-Shared/Components/errorBoundary';
 import { ProtectedRoutes } from './ignitus-Routes/protectedRoutes';
 // eslint-disable-next-line import/named
 import { PublicRoutes } from './ignitus-Routes/publicRoutes';
