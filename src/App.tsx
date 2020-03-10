db6d47a46f82f0fe5823129681d47147a4332c60
Rawat <divyanshu.r46956@gmail.com>
2020-03-10 21:45:10 +0100

    Deployment review test 🔨

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { withErrorBoundary } from './ignitus-Shared/Components/errorBoundary';
-import { ProtectedRoutes } from './ignitus-Routes/protectedRoutes';
+import {Switch, Route} from 'react-router-dom';
+import {withErrorBoundary} from './ignitus-Shared/Components/errorBoundary/index';
+import {ProtectedRoutes} from './ignitus-Routes/protectedRoutes';
 // eslint-disable-next-line import/named
-import { PublicRoutes } from './ignitus-Routes/publicRoutes';
+import {PublicRoutes} from './ignitus-Routes/publicRoutes';
 import './App.scss';
 
 const App = () => (
