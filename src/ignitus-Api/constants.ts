91cc0000f0afe335346d56fca4df9787678064a7
Rawat <divyanshu.r46956@gmail.com>
2020-03-11 19:47:38 +0100

    Contributors fetching url updates.

--- src/ignitus-Api/constants.ts
+++ src/ignitus-Api/constants.ts
@@ -4,9 +4,9 @@ export const TESTIMONIAL_URL = `${BASE_URL}/testimonials`;
 export const STUDENT_SIGN_UP = `${BASE_URL}/register/student`;
 export const STUDENT_SIGN_IN = `${BASE_URL}/login`;
 
-export const FRONTEND_CONTRIBUTOR_API = 'https://api.github.com/repos/Ignitus/Ignitus-Client-Side-Development/contributors?per_page=14';
+export const FRONTEND_CONTRIBUTOR_API = 'https://api.github.com/repos/Ignitus/Ignitus-Client-Side-Development/contributors?per_page=7';
 
-export const BACKEND_CONTRIBUTOR_API = 'https://api.github.com/repos/Ignitus/Ignitus-rest-api/';
+export const BACKEND_CONTRIBUTOR_API = 'https://api.github.com/repos/Ignitus/Ignitus-rest-api/contributors?per_page=7';
 
 export const CONTRIBUTORS_STORE = 'contributors';
 export const TESTIMONIALS_STORE = 'testimonials';
