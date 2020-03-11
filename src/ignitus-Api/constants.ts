a7f602fb3f1e9649aa32db72c3c0296f4138dae3
Shreya Kapoor <shreyakapoor98@gmail.com>
2020-03-11 19:50:46 +0530

    api link corrected

--- src/ignitus-Api/constants.ts
+++ src/ignitus-Api/constants.ts
@@ -4,7 +4,7 @@ export const TESTIMONIAL_URL = `${BASE_URL}/testimonials`;
 export const STUDENT_SIGN_UP = `${BASE_URL}/register/student`;
 export const STUDENT_SIGN_IN = `${BASE_URL}/login`;
 
-export const FRONTEND_CONTRIBUTOR_API = 'https://api.githubb.com/repos/Ignitus/Ignitus-Client-Side-Development/contributors?per_page=14';
+export const FRONTEND_CONTRIBUTOR_API = 'https://api.github.com/repos/Ignitus/Ignitus-Client-Side-Development/contributors?per_page=14';
 
 export const BACKEND_CONTRIBUTOR_API = 'https://api.github.com/repos/Ignitus/Ignitus-rest-api/contributors';
 
