cd514c0a9a297cb767cfdee8b8a2b010d28667c0
Rawat <divyanshu.r46956@gmail.com>
2020-03-12 22:54:19 +0100

    Deep scan fixes ☘️

--- src/ignitus-Shared/Components/ignitus-Navigation/Components/Navigation.test.js
+++ src/ignitus-Shared/Components/ignitus-Navigation/Components/Navigation.test.js
@@ -1,12 +1,6 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import logo from './Constants'
-import Foo from './Navigation';
-
-
 describe('Navigation', () => {
-	test('renders an image logo', () => {
-    expect(typeof 'https://storage.googleapis.com/ignitus/IgnitusLogos/white_logo.png').toBe('string')
+  test('renders an image logo', () => {
+    expect(typeof 'https://storage.googleapis.com/ignitus/IgnitusLogos/white_logo.png').toBe('string');
   });
 });
 
