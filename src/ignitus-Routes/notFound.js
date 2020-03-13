5628abfced52b9aa91bd68a58b41d87b65b9359a
Apoorva <apoorva.8765@gmail.com>
2020-03-13 11:47:41 +0530

    Revert "Updated UI and added Lottie Animation for 404 Not Found Page"
    This reverts commit bf88e3113ea752e9c5c880b5972dacf06f70902a.

--- src/ignitus-Routes/notFound.js
+++ src/ignitus-Routes/notFound.js
@@ -1,37 +1,20 @@
+
 import React from 'react';
 import './Styles/style.scss';
 import { Link } from 'react-router-dom';
-import Lottie from 'react-lottie'
-import animationData from '../ignitus-Routes/Styles/404animation.json';
-//path to animation  file
-const defaultOptions = 
-{
-      loop: true,
-      autoplay: true,
-      animationData: animationData,
-      rendererSettings: {
-        preserveAspectRatio: 'xMidYMid slice'
-      }
-};
-    export const Notfound = () =>
-     (<div>
-      <div className="container-404">
-      <div className="content">
-        <p ><b>Oops!</b></p>
-        <p>We can&apos;t seem to  find the page you were looking for&nbsp;</p>
-        <p>
-          Please check your URL or return&nbsp;
-          <Link to="/">
+
+export const Notfound = () => (
+  <div className="container-404">
+    <div className="content">
+      <p>404</p>
+      <p>We could not find what you were looking for.</p>
+      <p>
+		Please check your URL or return&nbsp;
+        <Link to="/">
           <span className="_link">home</span>
-          </Link>
-          .
-        </p>
-      </div>
-        {/* Lottie Animation */}
-      <div className="animation">
-        <Lottie options={defaultOptions}
-               height={400}
-               width={500}/>
-      </div>
-      </div>
-      </div>)
+        </Link>
+					.
+      </p>
+    </div>
+  </div>
+);
