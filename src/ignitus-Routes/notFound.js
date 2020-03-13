05fa594f3610f4e4171abb18fd799f82d463196d
Apoorva <apoorva.8765@gmail.com>
2020-03-13 09:54:17 +0530

    Revert "Recent Updates in Indentation"
    This reverts commit a4332c4266bf238b5a9eac0654493e2b1017b1bf.

--- src/ignitus-Routes/notFound.js
+++ src/ignitus-Routes/notFound.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import './Styles/style.scss';
 import { Link } from 'react-router-dom';
-import Lottie from 'react-lottie';
-import animationData from "./Styles/404animation.json";
+import Lottie from 'react-lottie'
+import animationData from '../ignitus-Routes/Styles/404animation.json';
 //path to animation  file
-const defaultOptions = {
-  loop: true,
-  autoplay: true,
-  animationData: animationData,
-  rendererSettings: {
-  preserveAspectRatio: 'xMidYMid slice'
-  }
+const defaultOptions = 
+{
+      loop: true,
+      autoplay: true,
+      animationData: animationData,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+      }
 };
-export const Notfound = () =>
-  (
-   <div>
-       <div className="container-404">
-        <div className="content">
-         <p ><b>Oops!</b></p>
-         <p>We can&apos;t seem to  find the page you were looking for&nbsp;</p>
-         <p>
+    export const Notfound = () =>
+     (<div>
+      <div className="container-404">
+      <div className="content">
+        <p ><b>Oops!</b></p>
+        <p>We can&apos;t seem to  find the page you were looking for&nbsp;</p>
+        <p>
           Please check your URL or return&nbsp;
           <Link to="/">
           <span className="_link">home</span>
@@ -28,11 +28,10 @@ export const Notfound = () =>
         </p>
       </div>
         {/* Lottie Animation */}
-        <div className="animation">
-         <Lottie options={defaultOptions}
-                height={400}
-                width={500}/>
+      <div className="animation">
+        <Lottie options={defaultOptions}
+               height={400}
+               width={500}/>
       </div>
       </div>
-     </div>
-  )
+      </div>)
