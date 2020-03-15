db682e3c28ac46e6b9c40f3dbf2a5ec2200621ae
Apoorva <apoorva.8765@gmail.com>
2020-03-15 10:03:20 +0530

    Added CSS Animation for Not Found Page

--- src/ignitus-Shared/Components/ignitus-NotFound/index.js
+++ src/ignitus-Shared/Components/ignitus-NotFound/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import './Styles/style.scss';
 import { Link } from 'react-router-dom';
-
 export const Notfound = () => (
-  <div className="container-404">
-    <div className="content">
-      <p>404</p>
-      <p>We could not find what you were looking for.</p>
-      <p>
-        Please check your URL or return&nbsp;
-        <Link to="/">
-          <span className="_link">home</span>
-        </Link>
-        .
-      </p>
+  <>
+    <div class="head">
+      <div className="meta"></div>
+      <div className="meta"></div>
     </div>
-  </div>
+    <div class="ani-main"></div>
+    <div class="ani-body" ></div>
+    <body>
+      <div className="container-404">
+        <div className="content">
+          <p>404</p>
+          <p>Looks like you're lost.</p>
+          <p>
+          The page you are looking for is not avaible!       
+          </p>
+          <p><a href="/" class="link_404">Go to Home</a></p>
+        </div>
+      </div>
+    </body>
+  </>
 );
