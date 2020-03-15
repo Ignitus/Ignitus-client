1b57f5b38106246e6b3a33dc185e10af030beadb
unknown <ashishnagpal2498@gmail.com>
2020-03-16 01:29:55 +0530

    :up: change to flex

--- src/ignitus-Shared/Components/ProfileCard/DropDownButton.js
+++ src/ignitus-Shared/Components/ProfileCard/DropDownButton.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import {withErrorBoundary} from '../errorBoundary';
-// import loader from '../../ignitus-Assets/Images/loader2.gif';
-// import * as S from './Styles';
-import './Styles/index.scss'
+import './Styles/index.scss';
 
 export const DropDownButton = () => (
-	<div className="btn-outer">
-	<div className="custom-dropdown-btn"> More ..
-	<ul className ="custom-dropdown-menu">
-		<li>Ask For Recommendation</li>
-		<li>Share Profile</li>
-		<li>Block</li>
-	</ul>
-	</div>
-	</div>
-)
+  <div className="btn-outer">
+    <div className="custom-dropdown-btn">
+      More ..
+      <ul className="custom-dropdown-menu">
+        <li>Ask For Recommendation</li>
+        <li>Share Profile</li>
+        <li>Block</li>
+      </ul>
+    </div>
+  </div>
+);
 export default withErrorBoundary(DropDownButton);
--- src/ignitus-Shared/Components/ProfileCard/index.js
+++ src/ignitus-Shared/Components/ProfileCard/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 import React from 'react';
-import { withErrorBoundary } from '../errorBoundary';
 import './Styles/index.scss';
 import DropDownButton from './DropDownButton';
 
@@ -13,21 +12,17 @@ export const SocialIcons = () => (
   </div>
 );
 
-export const ProfileCard = () => (
-  <ul style={ { margin: '150px 60px' } }>
-    <li className="list-item">
-      <div className="profile-details">
-        <div className="avatar-div">
-          <img
-            className="profile-avatar"
-            src={require('./dummy_image.png')}
-            alt="profile-avatar"
-          />
+export default function ProfileCard() {
+  return (
+    <div className="cardOuter">
+      <div className="cardMain">
+        <div className="cardImage">
+          <img src={require('./dummy_image.png')} alt="profile-avatar" />
         </div>
-        <div className="description">
-          <h2 className="profile-details-heading">Sophia Carter</h2>
+        <div className="cardInfo">
+          <h2>Sophia Carter</h2>
           <p>Literature Student</p>
-          <p className="text-icon">
+          <p className="textIcon">
             <span>
               <i className="fa fa-map-marker" />
             </span>
@@ -36,21 +31,29 @@ export const ProfileCard = () => (
           <DropDownButton />
         </div>
       </div>
-      <div className="profile-contact">
-        <SocialIcons />
-        <p className="text-icon contact-modes">
-          <span /> Send them a message
-        </p>
-        <p className="text-icon contact-modes">
-          <span />Resume
-        </p>
+      <div className="cardContact">
+        <div className="cardLinks">
+          <SocialIcons />
+        </div>
+        <div className="contactInfo">
+          <p className="textIcon">
+            <span>
+              <i className="fa fa-plus" />
+            </span>
+            Send them a message
+          </p>
+          <p className="textIcon">
+            <span>
+              <i className="fa fa-plus" />
+            </span>
+            Resume
+          </p>
+        </div>
+        <div className="trackPerson">
+          <p>Track Sophi&apos;s progress</p>
+          <i className="fa fa-bookmark" />
+        </div>
       </div>
-      <div className="track-person">
-        <p>Track Sophi&apos;s progress</p>
-        <i className="fa fa-bookmark" />
-      </div>
-    </li>
-  </ul>
-);
-
-export default withErrorBoundary(ProfileCard);
+    </div>
+  );
+}
