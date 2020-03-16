ff44e37e2f4a4f620af1e249d72819b46829541c
Rawat <divyanshu.r46956@gmail.com>
2020-03-16 22:04:30 +0100

    Further refactoring ☘️

--- src/ignitus-Shared/Components/UserSettingsSideNavigation/Components/index.tsx
+++ src/ignitus-Shared/Components/UserSettingsSideNavigation/Components/index.tsx
@@ -4,19 +4,31 @@ import * as S from '../style';
 
 type props = {
   text: string;
+  border?: boolean;
+  marginTop?: boolean;
+  marginBottom?: boolean;
 };
 
 export default function UserSettingsSideNavigation() {
   return (
     <S.SettingsContainer>
-      <Layer text="General" />
+      <Layer text="General" marginTop={true} />
       <Layer text="Password" />
       <Layer text="Messaging" />
       <Layer text="Notification" />
       <Layer text="Blocked Users" />
-      <Layer text="Privacy" />
+      <Layer text="Privacy" border={false} marginBottom={true} />
     </S.SettingsContainer>
   );
 }
 
-const Layer = ({ text }: props) => <S.Layer className="option general">{text}</S.Layer>;
+const Layer = ({
+  text,
+  border = true,
+  marginTop = false,
+  marginBottom = false,
+}: props) => (
+  <S.Layer border={border} marginTop={marginTop} marginBottom={marginBottom}>
+    {text}
+  </S.Layer>
+);
--- src/ignitus-Shared/Components/UserSettingsSideNavigation/style.ts
+++ src/ignitus-Shared/Components/UserSettingsSideNavigation/style.ts
@@ -1,20 +1,33 @@
 import styled from '@emotion/styled';
 import * as C from '../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/colors';
+import {flexibleColDiv} from '../../ignitus-DesignSystem/ignitus-Atoms/atoms';
+import {MD, Normal} from '../../ignitus-DesignSystem/ignitus-Atoms/fonts';
 
-export const SettingsContainer = styled.div`
+type Props = {
+  border: boolean;
+  marginTop: boolean;
+  marginBottom: boolean;
+};
+
+export const SettingsContainer = styled(flexibleColDiv)`
   background: ${C.White};
   border-radius: 16px;
   box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
-  padding: 1rem;
+  width: 12rem;
 `;
 
-export const Layer = styled.div`
-  cursor: pointer;
-  font-size: 16px;
-  font-weight: bold;
+export const Layer = styled.div<Props>`
+  border-bottom: ${props =>
+    props.border ? `1px solid ${C.GreyLight}` : 'null'};
   color: ${C.IgnitusBlue};
+  cursor: pointer;
+  font-size: ${MD};
+  font-weight: ${Normal};
   padding: 0.5rem;
-
+  margin-top: ${props => (props.marginTop ? '0.5rem' : 'none')};
+  margin-bottom: ${props => (props.marginBottom ? '0.5rem' : 'none')};
+  text-align: center;
+  width: 100%;
   &:hover {
     background-color: ${C.IgnitusBlue};
     color: ${C.White};
