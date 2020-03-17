d7bbfa47f258a9d59ef8df9e013d223e8067a95f
Rawat <divyanshu.r46956@gmail.com>
2020-03-17 20:47:18 +0100

    Migrated to emotion ☘️

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Layout/ignitus-NotFound/Components/index.tsx
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Layout/ignitus-NotFound/Components/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import '../style.scss';
 import * as S from '../style';
 
 export const Notfound = () => (
@@ -9,19 +8,14 @@ export const Notfound = () => (
       <S.Eyes />
       <S.Eyes />
     </S.EyesContainer>
-    <div className="ani-main" />
-    <div className="ani-body" />
-    <div className="container-404">
-      <div className="content">
-        <p>404</p>
-        <p>Looks like you're lost.</p>
-        <p>The page you are looking for is not avaible!</p>
-        <p>
-          <a href="/" className="link_404">
-            Go to Home
-          </a>
-        </p>
-      </div>
-    </div>
+    <S.AnimationText />
+    <S.MessageContainer>
+      <S.Message>
+        <S.StyledParagraph>Looks like you're lost.</S.StyledParagraph>
+        <S.StyledParagraph>
+          The page you are looking for is not available!
+        </S.StyledParagraph>
+      </S.Message>
+    </S.MessageContainer>
   </React.Fragment>
 );
--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Layout/ignitus-NotFound/style.ts
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Layout/ignitus-NotFound/style.ts
@@ -1,14 +1,48 @@
 import styled from '@emotion/styled';
+import {keyframes} from '@emotion/core';
 import * as C from '../../ignitus-Atoms/colors';
-import {flexibleColDiv} from '../../ignitus-Atoms/atoms';
 import {MD, Normal} from '../../ignitus-Atoms/fonts';
+import {Paragraph} from '../../ignitus-Atoms/atoms';
+
+export const eye = keyframes`
+  0% ,30% , 55%, 90% , 100% {
+    transform: translate(0, 0);
+  }
+  10%, 25% {
+    transform: translate(0, 20px);
+  }
+  65% {
+    transform: translate(-20px, 0);
+  }
+  80% {
+    transform: translate(20px, 0);
+  }
+`;
+
+export const shvr = keyframes`
+  0% {
+    transform: translate(1px, 1em);
+  }
+  50% {
+    transform: translate(0, 1em);
+  }
+  100% {
+    transform: translate(-1px, 1em);
+  }
+`;
+
+export const textShow = keyframes`
+  to {
+    text-indent: -373px;
+  }
+`;
 
 export const EyesContainer = styled.div`
   display: block;
   position: relative;
   width: 200px;
   margin: 10% auto 0;
-  animation: shvr 0.2s infinite;
+  animation: ${shvr} 0.2s infinite;
   height: 100%;
 
   &::after {
@@ -21,7 +55,7 @@ export const EyesContainer = styled.div`
     left: 25px;
     border-radius: 50%;
     box-shadow: 125px 0 0 white;
-    animation: eye 2.5s infinite;
+    animation: ${eye} 2.5s infinite;
   }
 `;
 
@@ -65,3 +99,43 @@ export const Eyes = styled.div`
     display: none;
   }
 `;
+
+export const AnimationText = styled.p`
+  margin-top: 100px;
+  text-align: center;
+  color: ${C.IgnitusBlue};
+  justify-content: center;
+  width: 100%;
+
+  &::before {
+    content: 'Got lost? How......?  Ahhhh...!';
+    color: ${C.IgnitusBlue};
+    width: 120px;
+    font-size: 30px;
+    overflow: hidden;
+    display: inline-block;
+    white-space: nowrap;
+    animation: ${textShow} 2s infinite steps(3);
+  }
+`;
+
+export const MessageContainer = styled.div`
+  min-height: 60vh;
+  padding: 20px;
+  display: inline;
+  justify-content: center;
+  align-items: center;
+  font-weight: ${C.IgnitusBlue};
+  text-align: center;
+`;
+
+export const Message = styled.p`
+  color: ${C.IgnitusBlue};
+  margin-left: 218px;
+  justify-content: center;
+  font-size: 22px;
+  display: inline;
+  text-align: center;
+`;
+
+export const StyledParagraph = styled(Paragraph)``;
