c1bfa431844e1fb82ee25d8a49ef3e5a32ee4fc6
Rawat <divyanshu.r46956@gmail.com>
2020-03-21 17:41:22 +0100

    Icons rendered using mapper ⚓️.

--- src/ignitus-UserInterfaceKit/Components/Atoms/interfaceIcons/Components/interfaceIcons.tsx
+++ src/ignitus-UserInterfaceKit/Components/Atoms/interfaceIcons/Components/interfaceIcons.tsx
@@ -2,28 +2,29 @@ import React from 'react';
 import * as I from '../styles';
 import {StyledHeading2} from '../../../../styles';
 import {AppIcon} from '../../../../../ignitus-Shared/types/iconsTypes/enums';
+import {
+  SocialIcons,
+  DashboardIcons,
+  GeneralIcons,
+  IgnitusLogos,
+} from '../constants';
 
 const interfaceIcons = () => (
   <I.IconsContainer>
     <StyledHeading2>Social Icons 🎨</StyledHeading2>
-    <I.StyledIcon name={AppIcon.LinkedInIcon} />
-    <I.StyledIcon name={AppIcon.AngelListIcon} />
-    <I.StyledIcon name={AppIcon.TwitterIcon} />
+    <IconsMapper icons={SocialIcons} />
     <hr />
     <StyledHeading2>Dashboard Icons</StyledHeading2>
-    <I.StyledIcon name={AppIcon.PlusIcon} />
-    <I.StyledIcon name={AppIcon.ApplyIcon} />
-    <I.StyledIcon name={AppIcon.HeartIcon} />
-    <I.StyledIcon name={AppIcon.MessageIcon} />
-    <I.StyledIcon name={AppIcon.SaveIcon} />
-    <I.StyledIcon name={AppIcon.NetworkIcon} />
-    <I.StyledIcon name={AppIcon.MentorsIcon} />
+    <IconsMapper icons={DashboardIcons} />
     <hr />
     <StyledHeading2>General Icons</StyledHeading2>
-    <I.StyledIcon name={AppIcon.KeyBoardArrowDown} />
-    <I.StyledIcon name={AppIcon.KeyBoardArrowRight} />
-    <I.StyledIcon name={AppIcon.Chevron} />
+    <IconsMapper icons={GeneralIcons} />
+    <hr />
+    <StyledHeading2>Ignitus Logos</StyledHeading2>
+    <IconsMapper icons={IgnitusLogos} />
   </I.IconsContainer>
 );
+
+const IconsMapper = ({icons}) => icons.map((icon: AppIcon) => <I.StyledIcon name={icon} />);
 
 export default interfaceIcons;
--- src/ignitus-UserInterfaceKit/Components/Layouts/interfaceUnderConstruction/Components/interfaceUnderConstruction.tsx
+++ src/ignitus-UserInterfaceKit/Components/Layouts/interfaceUnderConstruction/Components/interfaceUnderConstruction.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {StyledHeading2} from '../../../../styles';
-import {Notfound} from '../../../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Layout/ignitus-NotFound/Components/index';
 import {UnderConstructionContainer, Container} from '../style';
 import {WebsiteUnderConstruction} from '../../../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Layout/ignitus-UnderContruction/Components/index';
 
@@ -8,7 +7,6 @@ const interfaceUnderConstruction = () => (
   <Container>
     <StyledHeading2>UnderConstruction ★</StyledHeading2>
     <hr />
-
     <UnderConstructionContainer>
       <WebsiteUnderConstruction />
     </UnderConstructionContainer>
