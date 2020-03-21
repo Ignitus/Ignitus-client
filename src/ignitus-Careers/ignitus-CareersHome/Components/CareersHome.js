b56574c36fa12413bb4f592371eeb328324881fc
Rawat <divyanshu.r46956@gmail.com>
2020-03-21 21:13:56 +0100

    Fixes 🔨

--- src/ignitus-Careers/ignitus-CareersHome/Components/CareersHome.js
+++ src/ignitus-Careers/ignitus-CareersHome/Components/CareersHome.js
@@ -1,10 +1,7 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { withErrorBoundary } from '../../../ignitus-Shared/Components/errorBoundary';
+import {withErrorBoundary} from '../../../ignitus-Shared/Components/errorBoundary';
 import '../Styles/style.scss';
 
-const CareersHome = () => (
-<div className="outer-div-1">
-    Why Ignitus?
-</div>
-);
+const CareersHome = () => <div className="outer-div-1">Why Ignitus?</div>;
 export default withErrorBoundary(CareersHome);
--- src/ignitus-Careers/ignitus-CareersHome/Components/index.js
+++ src/ignitus-Careers/ignitus-CareersHome/Components/index.js
@@ -1,3 +1,3 @@
-/* eslint-disable import/prefer-default-export */
 
+// eslint-disable-next-line import/prefer-default-export
 export { default as CareersHome } from './CareersHome';
