777297b21b7be6d54f06ea4966b0d386496ffaa1
Rawat <divyanshu.r46956@gmail.com>
2020-03-21 18:07:45 +0100

    UI fixes notification added 🔨

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Organisms/ignitus-SideNavigation/ignitus-MessageSideNavigation/style.ts
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Organisms/ignitus-SideNavigation/ignitus-MessageSideNavigation/style.ts
@@ -1,7 +1,12 @@
 import styled from '@emotion/styled';
-import {White, IgnitusBlue, GreyLight} from '../../../ignitus-Atoms/colors';
+import {
+  White,
+  IgnitusBlue,
+  GreyLight,
+  Red,
+} from '../../../ignitus-Atoms/colors';
 import {flexibleColDiv} from '../../../ignitus-Atoms/atoms';
-import {MD, Normal} from '../../../ignitus-Atoms/fonts';
+import {XS, MD, Normal} from '../../../ignitus-Atoms/fonts';
 
 type Props = {
   border: boolean;
@@ -13,7 +18,7 @@ export const MessageContainer = styled(flexibleColDiv)`
   background: ${White};
   border-radius: 16px;
   box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
-  width: 12rem;
+  width: 14rem;
   height: 16rem;
 `;
 
@@ -26,10 +31,23 @@ export const Layer = styled.div<Props>`
   padding: 1rem;
   margin-top: ${props => (props.marginTop ? '0.5rem' : 'none')};
   margin-bottom: ${props => (props.marginBottom ? '0.5rem' : 'none')};
-  text-align: center;
   width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
   &:hover {
     background-color: ${IgnitusBlue};
     color: ${White};
   }
+`;
+
+export const Notification = styled(flexibleColDiv)`
+  border-radius: 100%;
+  padding: 0.2rem;
+  background: ${Red};
+  width: 1.5rem;
+  height: 1.5rem;
+  font-size: ${XS};
+  color: ${White}
 `;
