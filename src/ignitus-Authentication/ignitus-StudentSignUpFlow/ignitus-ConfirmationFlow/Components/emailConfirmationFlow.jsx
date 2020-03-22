d86bd81679fcfb17f4c64bf99b96066e8d92465e
Rawat <divyanshu.r46956@gmail.com>
2020-03-22 18:50:29 +0100

    Converted to .jsx  🔨

--- src/ignitus-Authentication/ignitus-StudentSignUpFlow/ignitus-ConfirmationFlow/Components/emailConfirmationFlow.jsx
+++ src/ignitus-Authentication/ignitus-StudentSignUpFlow/ignitus-ConfirmationFlow/Components/emailConfirmationFlow.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-filename-extension */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
