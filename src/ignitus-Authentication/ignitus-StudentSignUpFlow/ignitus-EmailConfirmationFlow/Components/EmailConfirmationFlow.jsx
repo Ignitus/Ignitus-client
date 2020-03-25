2dff49947a594933c7de2393be95ec54dc579f17
gargshruti30 <gargshruti30@gmail.com>
2020-03-26 00:11:36 +0530

    Email Confirmation Flow Page Migration

--- src/ignitus-Authentication/ignitus-StudentSignUpFlow/ignitus-EmailConfirmationFlow/Components/EmailConfirmationFlow.jsx
+++ src/ignitus-Authentication/ignitus-StudentSignUpFlow/ignitus-EmailConfirmationFlow/Components/EmailConfirmationFlow.jsx
@@ -1,28 +1,32 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import progress from '../../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Assets/ignitus-Images/img-Png/progressPage6.png';
 import newsletter from '../../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Assets/ignitus-Images/img-Png/newsletterAcceptIcon.png';
-import { withErrorBoundary } from '../../../../ignitus-Shared/ignitus-ErrorHandlingComponents/errorBoundary';
-import '../Styles/style.scss';
+import {
+  Paragraph,
+  Heading1,
+} from '../../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/typography';
+import {withErrorBoundary} from '../../../../ignitus-Shared/ignitus-ErrorHandlingComponents/errorBoundary';
+import * as T from '../../ignitus-WelcomeFlow/Styles/style';
+import * as C from '../Styles/style';
+import {RoundedPrimaryButton} from '../../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/buttons';
 
 const EmailConfirmationFlow = () => (
-  <div className="emailConfirm">
-    <img className="progressBarSignup" src={progress} alt="progress-bar" />
-    <div className="newsletter">
-      <img src={newsletter} alt="newsletter" />
-    </div>
-    <p className="welcome">Your email has been confirmed.</p>
-    <p className="paragraph">Your verified email is sophia.crater@gmail.com</p>
-    <div className="text-center">
-      <button className="getStarted" type="button">
-        <Link to="/" className="saveText">
-          Get started
-        </Link>
-      </button>
-    </div>
-  </div>
+  <T.WelcomeContainer>
+    <T.TopSection>
+      <T.Progress src={progress} alt="progress-bar" />
+      <C.NewsletterImage src={newsletter} alt="newsletter" />
+      <Heading1>Your email has been confirmed.</Heading1>
+      <Paragraph> Your verified email is sophia.crater@gmail.com</Paragraph>
+    </T.TopSection>
+    <C.BottomSection>
+      <RoundedPrimaryButton>
+        <Link to="/">Get started</Link>
+      </RoundedPrimaryButton>
+    </C.BottomSection>
+  </T.WelcomeContainer>
 );
 
 export default withErrorBoundary(EmailConfirmationFlow);
---
+++ src/ignitus-Authentication/ignitus-StudentSignUpFlow/ignitus-EmailConfirmationFlow/Styles/style.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+import {flexibleColDiv} from '../../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/atoms';
+
+export const NewsletterImage = styled.img``;
+
+export const BottomSection = styled.div`
+  margin-top: 2rem;
+`;
+
+export const TopSection = styled(flexibleColDiv)``;
