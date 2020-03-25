1ec83987de6aa46196d2974dc60eb5f42c7cb4dd
Rawat <divyanshu.r46956@gmail.com>
2020-03-25 22:17:38 +0100

    Minor updates 🙌🏻

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Organisms/ignitus-Navigation/Components/Navigation.jsx
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Organisms/ignitus-Navigation/Components/Navigation.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable react/prop-types */
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
@@ -9,67 +11,65 @@ import '../Styles/style.scss';
 const handleSmallerScreen = () => {
   const navlinks = document.querySelector('.navlinks');
   const lines = document.querySelectorAll('.line');
-  lines.forEach(line => {
+  lines.forEach((line) => {
     line.classList.toggle('mobile');
   });
   navlinks.classList.toggle('mobile');
-}
+};
 
 const PureNavigation = ({ displayClass, dynamicLogo }) => (
   <nav className={`navbar  ${displayClass}`}>
     <HashLink className="navbar-brand" to="/#">
       <img src={dynamicLogo} width="40" height="40" alt="logo" />
     </HashLink>
-    
-    <ul className='navlinks'>
-        <li className="navlinks-item active">
-          <HashLink smooth to='/#' className='navlink'>Home</HashLink>
-        </li>
-        
-        <li className='navlinks-item'>
-          <HashLink smooth to='/#what-we-do' className='navlink'>What we provide?</HashLink>
-        </li>
-        
-        <li className='navlinks-item'>
-          <HashLink smooth to='/#contributors' className='navlink'>Contributors</HashLink>
-        </li>
-        
-        <li className='navlinks-item'>
-          <Link to='/aboutus' className='navlink'>About</Link>
-        </li>
 
-        <li className='navlinks-item'>
-          <a
-            className='navlink'
-            rel='noopener noreferrer'
-            target='_blank'
-            href='https://bit.ly/2SaYXMO'
-          >
-                Join
-          </a>
-        </li>
+    <ul className="navlinks">
+      <li className="navlinks-item active">
+        <HashLink smooth to="/#" className="navlink">Home</HashLink>
+      </li>
 
-        <li className='navlinks-item'>
-          <Link to='/Login' className='navlink'>Sign in</Link>
-        </li>
+      <li className="navlinks-item">
+        <HashLink smooth to="/#what-we-do" className="navlink">What we provide?</HashLink>
+      </li>
 
-        <li className='navlinks-item'>
-          <Link to='/Signup' className='navlink'>Sign up</Link>
-        </li>
-      </ul>
-    
-      <div id="burger" onClick={handleSmallerScreen}>
-        <span className="line mobile"></span>
-        <span className="line mobile"></span>
-        <span className="line mobile"></span>
-      </div>
-    
+      <li className="navlinks-item">
+        <HashLink smooth to="/#contributors" className="navlink">Contributors</HashLink>
+      </li>
+
+      <li className="navlinks-item">
+        <Link to="/aboutus" className="navlink">About</Link>
+      </li>
+
+      <li className="navlinks-item">
+        <a
+          className="navlink"
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://bit.ly/2SaYXMO"
+        >
+          Join
+        </a>
+      </li>
+
+      <li className="navlinks-item">
+        <Link to="/Login" className="navlink">Sign in</Link>
+      </li>
+
+      <li className="navlinks-item">
+        <Link to="/Signup" className="navlink">Sign up</Link>
+      </li>
+    </ul>
+
+    <div id="burger" onClick={handleSmallerScreen}>
+      <span className="line mobile" />
+      <span className="line mobile" />
+      <span className="line mobile" />
+    </div>
   </nav>
 );
 
 
 const Navigation = () => {
-  
   const [navScrolled, setNavScrolled] = useState(false);
   const [displayClass, setDisplayClass] = useState('transparent');
   const [dynamicLogo, setDynamicLogo] = useState(blackLogo);
