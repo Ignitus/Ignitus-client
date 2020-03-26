84ce89fbf6fd2864acf64402be6ee0e0b2c6cf7b
Prahitha <prahitha.movva03@gmail.com>
2020-03-27 00:56:52 +0530

    Added opportunities saved layout

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Organisms/ignitus-OpportunitySaved/Components/OpportunitySaved.tsx
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Organisms/ignitus-OpportunitySaved/Components/OpportunitySaved.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import * as S from '../styles';
 import universityOfMelbourne from '../../../ignitus-Assets/ignitus-Images/img-Png/universityOfMelbourne.png'
 import { Description } from '../types';
-import { Data, Title, Subtitle, OpportunityDescription } from '../constants';
+import { opportunity } from '../constants';
 import {withErrorBoundary} from '../../../../ignitus-ErrorHandlingComponents/errorBoundary';
 
 export const OpportunitySavedCard = ({title, description, subtitle, data}: Description) => (
@@ -21,13 +21,6 @@ export const OpportunitySavedCard = ({title, description, subtitle, data}: Descr
     </S.ImageDescriptionContainer>
   </S.OpportunityContainer>
 );
-
-const opportunity = [{'title':Title[0],'description':OpportunityDescription[0],'subtitle':Subtitle[0], 'data':Data[0]},
-{'title':Title[1],'description':OpportunityDescription[0],'subtitle':Subtitle[0], 'data':Data[1]},
-{'title':Title[2],'description':OpportunityDescription[0],'subtitle':Subtitle[0], 'data':Data[2]},
-{'title':Title[1],'description':OpportunityDescription[0],'subtitle':Subtitle[0], 'data':Data[3]},
-{'title':Title[1],'description':OpportunityDescription[0],'subtitle':Subtitle[0], 'data':Data[4]},
-{'title':Title[3],'description':OpportunityDescription[0],'subtitle':Subtitle[0], 'data':Data[5]}];
 
 const OpportunitySavedLayout = () => (
   <S.OpportunityList>
--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Organisms/ignitus-OpportunitySaved/constants.ts
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Organisms/ignitus-OpportunitySaved/constants.ts
@@ -1,11 +1,6 @@
-export const universityOfMelbourne: string = '../../../../ignitus-Assets/ignitus-Images/img-Png/universityOfMelbourne.png';
-export const universityOfPortsmouth: string = '../../../../ignitus-Assets/ignitus-Images/img-Png/universityOfPortsmouth.png';
-export const tohokuUniversity: string = '../../../../ignitus-Assets/ignitus-Images/img-Png/tohokuUniversity.png';
-export const murdochUniversity: string = '../../../../ignitus-Assets/ignitus-Images/img-Png/murdochUniversity.png';
-export const microsoft: string = '../../../../ignitus-Assets/ignitus-Images/img-Png/microsoft.png';
-export const universityOfNewcastle: string = '../../../../ignitus-Assets/ignitus-Images/img-Png/universityOfNewcastle.png';
-
-export const Data: string[] = [universityOfMelbourne, universityOfPortsmouth, tohokuUniversity, murdochUniversity, microsoft, universityOfNewcastle,"https://brandhub.unimelb.edu.au/guidelines/logos/04_Logo_Vertical-Housed.jpg"];
-export const Title: string[] = ['PhD Project:', 'Internship', 'Funded Research', "Professor's Assistant"];
-export const OpportunityDescription: string[] = ["Church and civil courts in late medieval and early modern Scotland"]
-export const Subtitle: string[] = ["By The University of Melbourne"]
+export const opportunity = [{'title':'PhD Project','description':"Church and civil courts in late medieval and early modern Scotland",'subtitle': "By The University of Melbourne", 'data':'../../../../ignitus-Assets/ignitus-Images/img-Png/universityOfMelbourne.png'},
+{'title':"Internship",'description':"Church and civil courts in late medieval and early modern Scotland",'subtitle':"By The University of Melbourne", 'data':'../../../../ignitus-Assets/ignitus-Images/img-Png/universityOfPortsmouth.png'},
+{'title':"Funded Research",'description':"Church and civil courts in late medieval and early modern Scotland",'subtitle':"By The University of Melbourne", 'data':'../../../../ignitus-Assets/ignitus-Images/img-Png/tohokuUniversity.png'},
+{'title':"Internship",'description':"Church and civil courts in late medieval and early modern Scotland",'subtitle':"By The University of Melbourne", 'data':'../../../../ignitus-Assets/ignitus-Images/img-Png/murdochUniversity.png'},
+{'title':"Internship",'description':"Church and civil courts in late medieval and early modern Scotland",'subtitle':"By The University of Melbourne", 'data':'../../../../ignitus-Assets/ignitus-Images/img-Png/microsoft.png'},
+{'title':"Professor's Assistant",'description':"Church and civil courts in late medieval and early modern Scotland",'subtitle':"By The University of Melbourne", 'data':'../../../../ignitus-Assets/ignitus-Images/img-Png/universityOfNewcastle.png'}];
