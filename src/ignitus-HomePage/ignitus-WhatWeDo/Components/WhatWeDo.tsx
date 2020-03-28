ea4dd83e9af68b2eb27cba7c5656b5be31fea7d9
Rawat <divyanshu.r46956@gmail.com>
2020-03-28 17:52:48 +0100

    UI Updates 🔨

--- src/ignitus-HomePage/ignitus-WhatWeDo/Components/WhatWeDo.tsx
+++ src/ignitus-HomePage/ignitus-WhatWeDo/Components/WhatWeDo.tsx
@@ -3,12 +3,13 @@ import * as S from '../Styles';
 import * as T from '../../../ignitus-Shared/ignitus-DesignSystem/shared';
 import { withErrorBoundary } from '../../../ignitus-Shared/ignitus-ErrorHandlingComponents/errorBoundary';
 import { data } from '../constants';
+import { Paragraph } from '../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/typography';
 
 const WhatWeDo = () => (
   <T.Wrapper id="what-we-do">
     <T.TopSection>
-      <T.Title>What we provide?</T.Title>
-      <T.Paragraph>
+      <T.Title>What we Do?</T.Title>
+      <Paragraph>
         Guidance &nbsp;
         {' '}
         <S.Icon>
@@ -17,14 +18,14 @@ const WhatWeDo = () => (
         {' '}
         &nbsp; in well researched and goal-oriented courses, getting Internships
         etc.
-      </T.Paragraph>
+      </Paragraph>
     </T.TopSection>
     <T.BottomSection>
       <T.BottomRow>
         {[...data].map(x => (
           <S.Card key={x.src}>
             <S.CardPicture src={x.src} alt="logo" />
-            <S.CardText>{x.description}</S.CardText>
+            <Paragraph>{x.description}</Paragraph>
           </S.Card>
         ))}
       </T.BottomRow>
