6c3d639df79003f64c3ecf6dd4110b5287818d56
Rawat <divyanshu.r46956@gmail.com>
2020-03-29 15:40:28 +0200

    Logos removal from client-side 🔨

--- src/ignitus-Authentication/ignitus-StudentSignUpFlow/ignitus-InterestFlow/Components/InterestFlow.jsx
+++ src/ignitus-Authentication/ignitus-StudentSignUpFlow/ignitus-InterestFlow/Components/InterestFlow.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/extensions */
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Templates/ignitus-Authentication/constants.ts
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Templates/ignitus-Authentication/constants.ts
@@ -1,5 +1,5 @@
 export const logo: string =
-  'https://storage.googleapis.com/ignitus_assets/ig-logo/ignitusBlue';
+  'https://storage.googleapis.com/ignitus_assets/ig-logo/ignitusBlue.svg';
 export const studentAuth: string =
   'https://storage.googleapis.com/ignitus_assets/ig-assets/Assets_studentAuth.svg';
 export const professorAuth: string =
