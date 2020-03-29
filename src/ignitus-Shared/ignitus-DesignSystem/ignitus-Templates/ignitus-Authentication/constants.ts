05a7a3efabcb4ff513b7ff74fe5cfc4446797364
Rawat <divyanshu.r46956@gmail.com>
2020-03-29 16:09:35 +0200

    Fixes 🔨

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Templates/ignitus-Authentication/constants.ts
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Templates/ignitus-Authentication/constants.ts
@@ -1,6 +1,4 @@
 export const logo: string =
   'https://storage.googleapis.com/ignitus_assets/ig-logo/ignitusBlue.svg';
-export const studentAuth: string =
-  'https://storage.googleapis.com/ignitus_assets/ig-assets/Assets_studentAuth.svg';
-export const professorAuth: string =
-  'https://storage.googleapis.com/ignitus_assets/ig-assets/Assets_professorAuth.svg';
+  export const studentAuth: string = 'https://storage.googleapis.com/ignitus_assets/ig-assets/studentAuthentication.svg';
+  export const professorAuth: string = 'https://storage.googleapis.com/ignitus_assets/ig-assets/professorAuthentication.svg';
