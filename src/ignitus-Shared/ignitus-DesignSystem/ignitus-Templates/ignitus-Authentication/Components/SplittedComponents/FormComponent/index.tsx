5c616004d9cea9a462f9b5fce8eea682c2b9d72c
Rawat <divyanshu.r46956@gmail.com>
2020-03-30 22:52:40 +0200

    DeepScan Fixes 🔨

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Templates/ignitus-Authentication/Components/SplittedComponents/FormComponent/index.tsx
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Templates/ignitus-Authentication/Components/SplittedComponents/FormComponent/index.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent, ComponentState} from 'react';
+import React, {FunctionComponent} from 'react';
 import {Link} from 'react-router-dom';
 
 import {isEmpty} from '../../../../../../ignitus-Utilities/HelperFunctions/lodashHelpers';
