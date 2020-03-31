c87e151f5fb3ce1a6a16020ac07add42c69d64b5
Asfiya Shaikh <aysp.1999@gmail.com>
2020-03-31 22:16:06 +0530

    Added functionality->Nav and minor fixes

--- src/ignitus-HomePage/ignitus-Testimonial/Components/Testimonial.jsx
+++ src/ignitus-HomePage/ignitus-Testimonial/Components/Testimonial.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, createRef} from 'react';
 import {withErrorBoundary} from '../../../ignitus-Shared/ignitus-ErrorHandlingComponents/errorBoundary';
 import '../Styles/style.scss';
 import i1 from '../images/i1.png';
@@ -11,27 +11,22 @@ import i4 from '../images/i4.png';
 
 const Testimonial = ({testimonialData, getTestimonialData}) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const nextTestimonial = () => {
-    setCurrentIndex(previous => {
-      if (previous === testimonialData.length - 1) {
-        return 0;
-      }
-
-      return previous + 1;
-    });
+  let handleClick = event => {
+    const id = event.target.id;
+    if (id === 'firstImage') {
+      setCurrentIndex(0);
+    } else if (id === 'secondImage') {
+      setCurrentIndex(1);
+    } else if (id === 'thirdImage') {
+      setCurrentIndex(2);
+    } else if (id === 'fourthImage') {
+      setCurrentIndex(3);
+    }
   };
 
   useEffect(() => {
     getTestimonialData();
   }, []);
-
-  const prevTestimonial = () => {
-    let nextIndex = currentIndex - 1;
-    if (currentIndex === 0) {
-      nextIndex = testimonialData.length - 1;
-    }
-    setCurrentIndex(nextIndex);
-  };
 
   return (
     <div className="testimonial">
@@ -39,9 +34,9 @@ const Testimonial = ({testimonialData, getTestimonialData}) => {
       <div className="testimonial-wrapper">
         {testimonialData.length > 0 && (
           <div className="carousel-content">
-            <blockquote className="text">
+            <div className="text-box">
               <p>{testimonialData[currentIndex].description}</p>
-            </blockquote>
+            </div>
             <div className="avatar">
               <img src={i1} />
             </div>
@@ -52,10 +47,10 @@ const Testimonial = ({testimonialData, getTestimonialData}) => {
           </div>
         )}
         <div className="nav-avatar">
-          <img src={i1} onClick={nextTestimonial} />
-          <img src={i2} />
-          <img src={i3} />
-          <img src={i4} onClick={prevTestimonial} />
+          <img id="firstImage" src={i1} onClick={handleClick} />
+          <img id="secondImage" src={i2} onClick={handleClick} />
+          <img id="thirdImage" src={i3} onClick={handleClick} />
+          <img id="fourthImage" src={i4} onClick={handleClick} />
         </div>
       </div>
     </div>
