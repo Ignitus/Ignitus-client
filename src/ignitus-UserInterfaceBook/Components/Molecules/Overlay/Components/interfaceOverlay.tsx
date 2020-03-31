512aa444d03f8356277ff1e6e0319bc1418f293b
nikitaghps <nikitaghps@gmail.com>
2020-03-31 15:38:32 +0530

    material-ui removed

--- src/ignitus-UserInterfaceBook/Components/Molecules/Overlay/Components/interfaceOverlay.tsx
+++ src/ignitus-UserInterfaceBook/Components/Molecules/Overlay/Components/interfaceOverlay.tsx
@@ -1,42 +1,36 @@
 import React from 'react';
 import {TypographyContainer} from '../styles';
 import {
-  Heading5,
+  Heading1,
   Paragraph,
   TextLink,
 } from   '../../../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/typography';
 import * as Button from '../../../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/buttons';
 import confetti from '../../../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Assets/ignitus-Logos/logo-Svg/confetti.svg';
-import { Backdrop } from '@material-ui/core';
-// const [open, setOpen] = React.useState(false);
 
 const interfaceOverlay = () => (
-  // <Backdrop  open={true} >
+
   <TypographyContainer>
-    <Backdrop open={true}  style ={ { backgroundImage: `url(${confetti})`,backgroundColor:'white',border:"solid 2px black",  backgroundSize: 'cover', width: '77%',
-  height: '500px','borderRadius':'45px', left: '22%', top:'10%'} }>
-    <div style={{   position: 'absolute', //left: '45%', top: '35%'
+    <div  style ={ { backgroundImage: `url(${confetti})`,backgroundColor:'white',border:"solid 2px black",  backgroundSize: 'cover', width: '100%',
+  height: '500px','borderRadius':'45px', left: '%', top:'10%'} }>
+    <div style={{   position: 'absolute', left: '47%', top: '35%'
          }}>
            <div style={{ display:'flex', alignItems:'center', justifyContent:'center'}}>
-    <Heading5>Hooray!</Heading5>
+    <Heading1 style={{fontSize:'48px',fontWeight:900}}>Hooray!</Heading1>
     </div>
     <div style={{ display:'flex', alignItems:'center', justifyContent:'center'}}>
-    <Paragraph>Your application has been sent successfully.</Paragraph>
+    <Paragraph style={{fontSize:'18px',fontWeight:900}}>Your application has been sent successfully.</Paragraph>
     </div>
     <div style={{ display:'flex', alignItems:'center', justifyContent:'center'}}>
-    <TextLink>Make sure to keep an eye on your email.</TextLink>
+    <TextLink style={{fontSize:'14px'}}>Make sure to keep an eye on your email.</TextLink>
     </div>
     </div>
-    {/* <div style={{ position: 'absolute', left: '83%', top: '80%',zIndex: 10}} > */}
-    <Button.RoundedPrimaryButton  style={{ zIndex:1, left: '83%', top: '80%',position:'absolute'}} >
+    <Button.RoundedPrimaryButton  style={{ zIndex:1, left: '80%', top: '80%',position:'relative',backgroundColor:'#E9A447'}} >
       {' '}
       Go Back{' '}
     </Button.RoundedPrimaryButton>
-    {/* </div> */}
-    </Backdrop>
-    
+    </div>
   </TypographyContainer>
-  // </Backdrop>
 );
 
 export default interfaceOverlay;
