2067843364f108c36b63c30b09dde5a4fd4bd3db
Rawat <divyanshu.r46956@gmail.com>
2020-04-06 18:27:09 +0200

    Refactoring 🔨

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/ignitus-defaultIconInput/Components /defaultIconInput.tsx
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/ignitus-defaultIconInput/Components /defaultIconInput.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {AppIcon} from '../../../../types/iconsTypes/enums';
 import * as I from '../styles';
+import {AppIcon} from '../../../../types/iconsTypes/iconEnums';
 
 const DefaultIconInput = () => (
   <React.Fragment>
     <I.InputContainer>
-      <I.Input> https://goo.gl/HEp5FW </I.Input >
+      <I.Input> https://goo.gl/HEp5FW </I.Input>
       <I.StyledIcon name={AppIcon.CopyIcon} />
     </I.InputContainer>
   </React.Fragment>
