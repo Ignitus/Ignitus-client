7413d3a6925e9ffd7439db300e8dfa42bdff84d2
Marvin9 <marvinduff97@gmail.com>
2020-04-08 14:38:45 +0530

    Fix auto assign

--- scripts/verify.js
+++ scripts/verify.js
@@ -4,14 +4,14 @@ const YAML = require('yaml');
 
 const [,, user, command] = process.argv;
 
-const autoAssign = fs.readFileSync('./.github/auto_assign.yml');
+const autoAssign = fs.readFileSync('./.github/auto_assign.yml', 'utf8');
 const authorizedUsers = YAML.parse(autoAssign).reviewers;
 
 if (user && command) {
   const commandFormat = command.split(' ').join('').trim().toLocaleLowerCase();
   if (commandFormat === 'bottestthis') {
     const isAuthorized = authorizedUsers.findIndex(
-      assignedUser => assignedUser === authorizedUsers,
+      assignedUser => assignedUser === user,
     );
     if (isAuthorized === -1) process.exit(1);
   } else process.exit(1);
