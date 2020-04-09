bb387c5b8112960f2036330c70dc7d169804c42c
Rawat <divyanshu.r46956@gmail.com>
2020-04-09 03:36:56 +0200

    AboutUs cleanup 🧹

--- src/ignitus-AboutPage/ignitus-About/Components/About.tsx
+++ src/ignitus-AboutPage/ignitus-About/Components/About.tsx
@@ -1,10 +1,9 @@
 import React, {Fragment} from 'react';
 import Team from '../../ignitus-Team';
 import CoreTeam from '../../ignitus-CoreTeam';
-import * as t from '../constants';
-import * as S from '../Styles';
-import {Paragraph} from '../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/typography';
-import {withErrorBoundary} from '../../../ignitus-Shared/ignitus-ErrorHandlingComponents/errorBoundary';
+import { IgnitusIntroduction, AboutImage } from '../constants';
+import { withErrorBoundary } from '../../../ignitus-Shared/ignitus-ErrorHandlingComponents/errorBoundary';
+import * as S from '../styles';
 
 const About: React.FunctionComponent = () => (
   <Fragment>
@@ -12,12 +11,12 @@ const About: React.FunctionComponent = () => (
       <S.AboutContainer>
         <S.TopSection>
           <S.PictureContainer>
-            <S.Img src={t.aboutImg} alt="aboutus" />
+            <S.Img src={AboutImage} alt="aboutus" />
           </S.PictureContainer>
           <S.ContentContainer>
             <S.Title> About Us</S.Title>
             <S.ParagraphContainer>
-              {t.IgnitusIntro.map((x: any) => (
+              {IgnitusIntroduction.map((x: any) => (
                 <S.Paragraph key={x.title}>{x.title}</S.Paragraph>
               ))}
             </S.ParagraphContainer>
--- src/ignitus-AboutPage/ignitus-About/constants.ts
+++ src/ignitus-AboutPage/ignitus-About/constants.ts
@@ -1,12 +1,9 @@
-/* eslint-disable eol-last */
 // eslint-disable-next-line import/prefer-default-export
-export const aboutImg: string = 'https://storage.googleapis.com/ignitus_assets/ig-assets/about.png';
 
-interface IntroObj {
-  title: string,
-}
+import { IntroductionType } from "./types";
 
-export const IgnitusIntro: IntroObj[] = [
+export const AboutImage: string = 'https://storage.googleapis.com/ignitus_assets/ig-assets/about.png';
+export const IgnitusIntroduction: IntroductionType[] = [
   { title: 'Ignitus a non-profit organization that helps students and professionals get handpicked top quality global research and industrial internships, for free!' },
   { title: 'Our only goal is skyrocketing a students career by providing best global opportunities. The platform has got exponential growth after successful internship completion by the associated students with researchers from SAIL (Stanford AI Lab) and Oxford University.' },
   { title: 'Ignitus scholar consists of 200+ applicants who have worked at Mercedes-Benz, Harvard Innovation Labs, UC Berkeley etc. Ignitus is made with love from students, researchers & alumni of Stanford University, Massachusetts Institute of Technology, Princeton University, Georgia Tech University, Harvard University, Oxford University, University of California, Berkeley etc. 🎓' },
