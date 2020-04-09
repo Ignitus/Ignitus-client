a4bd247f6a64fefcd671fd656468723a285c5c0e
Rawat <divyanshu.r46956@gmail.com>
2020-04-09 02:36:05 +0200

    Updates 🔨

--- src/ignitus-AboutPage/ignitus-About/constants.ts
+++ src/ignitus-AboutPage/ignitus-About/constants.ts
@@ -9,6 +9,6 @@ interface IntroObj {
 export const IgnitusIntro: IntroObj[] = [
   { title: 'Ignitus a non-profit organization that helps students and professionals get handpicked top quality global research and industrial internships, for free!' },
   { title: 'Our only goal is skyrocketing a students career by providing best global opportunities. The platform has got exponential growth after successful internship completion by the associated students with researchers from SAIL (Stanford AI Lab) and Oxford University.' },
-  { title: 'Ignitus scholar consists of 200+ applicants who have worked at Mercedes-Benz, Harvard Innovation Labs, UC Berkeley etc. Ignitus is made with love from students, researchers & alumni of Stanford University, Massachusetts Institute of Technology, Princeton University, Georgia Tech University, Harvard University, Oxford University, University of California, Berkeley etc.' },
-  { title: 'Team Ignitus boasts of a dedicated workforce from Pittsburgh, Barcelona, Munich, Berkeley and different parts of the world 🌍.' }
+  { title: 'Ignitus scholar consists of 200+ applicants who have worked at Mercedes-Benz, Harvard Innovation Labs, UC Berkeley etc. Ignitus is made with love from students, researchers & alumni of Stanford University, Massachusetts Institute of Technology, Princeton University, Georgia Tech University, Harvard University, Oxford University, University of California, Berkeley etc. 🎓' },
+  { title: 'Team Ignitus boasts of a dedicated workforce from Pittsburgh, Barcelona, Munich, Berkeley etc.' }
 ];
