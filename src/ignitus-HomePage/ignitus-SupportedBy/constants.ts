df1fa4d86e5361dea01bbcbee43320b10726cf3f
Rawat <divyanshu.r46956@gmail.com>
2020-04-09 19:27:32 +0200

    Netlify Updates 🔨

--- src/ignitus-HomePage/ignitus-SupportedBy/constants.ts
+++ src/ignitus-HomePage/ignitus-SupportedBy/constants.ts
@@ -1,7 +1,7 @@
 export const WOOTECH: string = 'https://storage.googleapis.com/ignitus_assets/ig-partners-logo/WooTech.svg';
 export const SUGARLABS: string = 'https://storage.googleapis.com/ignitus_assets/ig-partners-logo/sugarlabs.png';
 export const SYSTERS: string = 'https://storage.googleapis.com/ignitus_assets/ig-partners-logo/railsGirlsSummerOfCode.png';
-export const NETLIFY: string = 'https://www.netlify.com/img/press/logos/logomark-light.svg';
+export const NETLIFY: string = 'https://www.netlify.com/img/global/badges/netlify-color-accent.svg';
 export const GIRLSCRIPT: string = 'https://storage.googleapis.com/ignitus_assets/ig-partners-logo/girlScriptLogo.png';
 export const PROGATE: string = 'https://storage.googleapis.com/ignitus_assets/ig-partners-logo/progateLogo.png';
 
