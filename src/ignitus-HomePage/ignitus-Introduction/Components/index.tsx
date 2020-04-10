faeb009a257d820faf268d3b34e876d8e3162787
Rawat <divyanshu.r46956@gmail.com>
2020-04-10 12:12:41 +0200

    UI updates 🔨

--- src/ignitus-HomePage/ignitus-Introduction/Components/index.tsx
+++ src/ignitus-HomePage/ignitus-Introduction/Components/index.tsx
@@ -1,42 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import '../Styles/style.scss';
 import * as T from '../constants';
-import * as I from '../styles'
-import { withErrorBoundary } from '../../../ignitus-Shared/ignitus-ErrorHandlingComponents/errorBoundary';
+import * as I from '../styles';
+import {withErrorBoundary} from '../../../ignitus-Shared/ignitus-ErrorHandlingComponents/errorBoundary';
 
 export const Introduction: React.FunctionComponent = withErrorBoundary(() => (
   <I.Container>
-    <img className="intro__torch" src={T.TORCH} alt="Ignitus Torch" />
-    <h1 className="intro text-center">Welcome to Ignitus</h1>
-
-    <div className="intro__content">
-      <div className="intro__content--upper">
+    {/* <I.Torch src={T.TORCH} alt="Ignitus Torch" /> */}
+    <I.LeftSection>
+      <I.Title>Welcome to Ignitus</I.Title>
+      <I.PrimaryText>
         Get handpicked top-quality global research <br />
         and industrial internships, for <span className="bold-txt">free</span>!
-      </div>
+      </I.PrimaryText>
+      <I.SecondaryText>
+        Ignite your resume and get the{' '}
+        <span className="bold-txt">
+          best global <br />
+          opportunities
+        </span>{' '}
+        and Skyrocketing your career
+      </I.SecondaryText>
+    </I.LeftSection>
 
-      <div className="intro__content--lower">
-        <div className="content--lower__signup">
-          <p>
-            Ignite your resume and get the{' '}
-            <span className="bold-txt">
-              best global <br />
-              opportunities
-            </span>{' '}
-            and Skyrocketing your career
-          </p>
-          <Link to="/Signup">
-            <button
-              type="button"
-              className="btn btn-secondary button-get-started"
-            >
-              Sign Up
-            </button>
-          </Link>
-        </div>
-        <img className="img-fluid" src={T.RESUME} alt="resume" />
-      </div>
-    </div>
+    <I.RightSection />
   </I.Container>
 ));
--- src/ignitus-HomePage/ignitus-Introduction/styles.ts
+++ src/ignitus-HomePage/ignitus-Introduction/styles.ts
@@ -1,29 +1,49 @@
 import styled from '@emotion/styled';
+import * as C from '../../ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/colors';
+import {flexibleRowDiv} from '../../ignitus-Shared/ignitus-DesignSystem/shared';
+import {
+  XL,
+  LG,
+  XXL,
+} from '../../ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/fonts';
 
-export const Container = styled.div`
-  background: url('https://storage.googleapis.com/ignitus_assets/ig-assets/splitBlueBackground.png')
-    no-repeat;
+const ContainerBackground: string =
+  'https://storage.googleapis.com/ignitus_assets/ig-assets/splitBlueBackground.png';
+
+export const Container = styled(flexibleRowDiv)`
+  background: url(${ContainerBackground}) no-repeat;
   background-size: 70%;
   background-position: top right;
-  height: 67.5rem;
-  position: relative;
-  z-index: 0;
+  padding: 4rem;
+`;
 
-  @media (max-width: 945px) {
-    & {
-      height: 75rem;
-    }
-  }
+export const Title = styled.h1`
+  color: ${C.IgnitusBlue};
+  font-size: 64px;
+  font-weight: 500;
+  letter-spacing: 4px;
+`;
 
-  @media (max-width: 751px) {
-    & {
-      height: 100%;
-    }
-  }
+export const PrimaryText = styled.p`
+  color: ${C.Black};
+  font-size: ${XL};
+  letter-spacing: 4px;
+`;
 
-  @media (max-width: 568px) {
-    & {
-      margin-bottom: 0;
-    }
-  }
+export const SecondaryText = styled.p`
+  color: ${C.Black};
+  font-size: ${LG};
+  letter-spacing: 4px;
+`;
+
+export const LeftSection = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const RightSection = styled.h1`
+  color: ${C.IgnitusBlue};
+  font-size: 78px;
+  font-weight: 500;
+  letter-spacing: 4px;
 `;
