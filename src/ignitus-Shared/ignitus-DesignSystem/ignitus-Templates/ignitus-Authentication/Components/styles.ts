01622d6e7d05e2815ccb620cccffbd0a234d00f5
Rawat <divyanshu.r46956@gmail.com>
2020-04-13 19:24:17 +0200

    UI Updates 🔨

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Templates/ignitus-Authentication/Components/styles.ts
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Templates/ignitus-Authentication/Components/styles.ts
@@ -1,7 +1,51 @@
 import styled from '@emotion/styled';
-export const Container = styled.div`
+import {
+  BootStrapSection,
+  flexibleRowDiv,
+  flexibleColDiv,
+} from '../../../shared';
+import {SecondaryColor, IgnitusBlue} from '../../../ignitus-Atoms/colors';
+import {Paragraph as P} from '../../../ignitus-Atoms/typography';
+import { Bold } from '../../../ignitus-Atoms/fonts';
+
+export const AuthenticationWrapper = styled.div`
   display: flex;
-  align-items: center;
   justify-content: center;
-  height: 46vw;
+  align-items: center;
+  padding: 4rem;
+`;
+
+export const Container = styled(BootStrapSection)`
+  margin: 2rem auto;
+`;
+
+export const Row = styled(flexibleRowDiv)``;
+
+export const Paragraph = styled(P)`
+  text-align: center;
+  margin: 0.5rem 0;
+  font-weight: 500;
+
+  a {
+    color: ${IgnitusBlue};
+    font-weight: ${Bold};
+  }
+`;
+
+export const LeftRow = styled(flexibleColDiv)`
+  background: ${SecondaryColor};
+  padding: 2rem;
+`;
+
+export const ColumnOne = styled.div``;
+export const ColumnTwo = styled(flexibleColDiv)`
+  padding: 1rem;
+`;
+
+export const Image = styled.img`
+  width: 16rem;
+`;
+
+export const RightRow = styled(flexibleColDiv)`
+  padding: 2rem;
 `;
