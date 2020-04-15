e205a6b1be2568e410cbf18f5dfbc595e290926c
Rawat <divyanshu.r46956@gmail.com>
2020-04-15 18:49:27 +0200

    Navigation updates 🎯

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Organisms/ignitus-SideNavigation/ignitus-DashboardSideNavigation/style.ts
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Organisms/ignitus-SideNavigation/ignitus-DashboardSideNavigation/style.ts
@@ -1,9 +1,13 @@
 import styled from '@emotion/styled';
-import {White, IgnitusBlue, GreyLight, boxShadowColor} from '../../../ignitus-Atoms/colors';
+import {
+  White,
+  IgnitusBlue,
+  GreyLight,
+  boxShadowColor,
+} from '../../../ignitus-Atoms/colors';
 import {MD, Normal} from '../../../ignitus-Atoms/fonts';
-import { flexibleColDiv } from '../../../shared';
+import {flexibleColDiv} from '../../../shared';
 import Icon from '../../../../ignitus-Utilities/Components/icon';
-
 
 type Props = {
   border: boolean;
@@ -15,22 +19,22 @@ export const SettingsContainer = styled(flexibleColDiv)`
   background: ${White};
   border-radius: 16px;
   box-shadow: 0 2px 4px 0 ${boxShadowColor};
-  width: 12rem;
-  height: 24rem;
 `;
 
 export const Layer = styled.div<Props>`
   border-bottom: ${props => (props.border ? `1px solid ${GreyLight}` : 'null')};
   color: ${IgnitusBlue};
   cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
   font-size: ${MD};
   font-weight: ${Normal};
-  padding: 1rem;
   margin-top: ${props => (props.marginTop ? '0.5rem' : 'none')};
   margin-bottom: ${props => (props.marginBottom ? '0.5rem' : 'none')};
-  display: flex;
+  width: 8rem;
+  padding: 1rem;
   flex-direction: row;
-  width: 100%;
   &:hover {
     background-color: ${IgnitusBlue};
     color: ${White};
