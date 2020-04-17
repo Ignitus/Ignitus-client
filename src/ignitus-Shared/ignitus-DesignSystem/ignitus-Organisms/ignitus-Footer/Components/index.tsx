d1d5bab507416a780a813a7e1ba7a43f460aed4e
Rawat <divyanshu.r46956@gmail.com>
2020-04-17 22:14:25 +0200

    Replaced font-awesome => svg.

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Organisms/ignitus-Footer/Components/index.tsx
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Organisms/ignitus-Footer/Components/index.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable import/extensions */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {HashLink} from 'react-router-hash-link';
-import { withErrorBoundary } from '../../../../ignitus-ErrorHandlingComponents/errorBoundary';
+import {withErrorBoundary} from '../../../../ignitus-ErrorHandlingComponents/errorBoundary';
 
-import { Props } from '../types';
-import { AppIcon } from '../../../../types/iconsTypes/iconEnums';
+import {Props} from '../types';
+import {AppIcon} from '../../../../types/iconsTypes/iconEnums';
 
 import * as S from '../styles';
 
@@ -24,10 +24,22 @@ const PureFooter: React.FunctionComponent<Props> = ({
           Features
         </S.StyledHeading>
         <S.FooterLinks toggle={featuresToggle}>
-          <li> <a href="#!">Resume</a> </li>
-          <li> <a href="#!">Opportunities</a></li>
-          <li> <a href="#!">Interships</a> </li>
-          <li> <a href="#!">OpenSource</a> </li>
+          <li>
+            {' '}
+            <a href="#!">Resume</a>{' '}
+          </li>
+          <li>
+            {' '}
+            <a href="#!">Opportunities</a>
+          </li>
+          <li>
+            {' '}
+            <a href="#!">Interships</a>{' '}
+          </li>
+          <li>
+            {' '}
+            <a href="#!">OpenSource</a>{' '}
+          </li>
         </S.FooterLinks>
       </S.FooterSections>
 
@@ -36,10 +48,34 @@ const PureFooter: React.FunctionComponent<Props> = ({
           Community
         </S.StyledHeading>
         <S.FooterLinks toggle={communityToggle}>
-           <li> <a href="#!">Personal</a> </li>
-           <li> <a href="#!">Partners</a> </li>
-           <li> <a target="_blank" rel="noopener noreferrer" href="https://www.quora.com/What-are-Ignitus-and-WooTech-about">Ignitus Woo-Tech</a> </li>
-           <li> <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@afelio_22020/introducing-ignitus-scholar-6b0c677ba9d7">Ignitus Scholar</a> </li>
+          <li>
+            {' '}
+            <a href="#!">Personal</a>{' '}
+          </li>
+          <li>
+            {' '}
+            <a href="#!">Partners</a>{' '}
+          </li>
+          <li>
+            {' '}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.quora.com/What-are-Ignitus-and-WooTech-about"
+            >
+              Ignitus Woo-Tech
+            </a>{' '}
+          </li>
+          <li>
+            {' '}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://medium.com/@afelio_22020/introducing-ignitus-scholar-6b0c677ba9d7"
+            >
+              Ignitus Scholar
+            </a>{' '}
+          </li>
         </S.FooterLinks>
       </S.FooterSections>
       <S.FooterSections>
@@ -51,9 +87,36 @@ const PureFooter: React.FunctionComponent<Props> = ({
           Company
         </S.StyledHeading>
         <S.FooterLinks toggle={companyToggle}>
-        <li> <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/Ignitus.org/">About</a> </li>
-        <li> <a target="_blank" rel="noopener noreferrer" href="https://angel.co/ignitus-2/jobs">Careers</a> </li>
-        <li> <a target="_blank" rel="noopener noreferrer" href="https://medium.com/swlh/ignitus-connecting-students-with-opportunities-61f08e19861c">Blog</a> </li>
+          <li>
+            {' '}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.facebook.com/Ignitus.org/"
+            >
+              About
+            </a>{' '}
+          </li>
+          <li>
+            {' '}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://angel.co/ignitus-2/jobs"
+            >
+              Careers
+            </a>{' '}
+          </li>
+          <li>
+            {' '}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://medium.com/swlh/ignitus-connecting-students-with-opportunities-61f08e19861c"
+            >
+              Blog
+            </a>{' '}
+          </li>
         </S.FooterLinks>
       </S.FooterSections>
 
@@ -62,31 +125,123 @@ const PureFooter: React.FunctionComponent<Props> = ({
           Links
         </S.StyledHeading>
         <S.FooterLinks toggle={linkToggle}>
-        <li> <HashLink to="/#!">Home</HashLink> </li>
-        <li> <HashLink to="/#what-we-do">What we Do?</HashLink> </li>
-        <li> <HashLink to="/interface">Design System</HashLink> </li>
-        <li> <HashLink to="/#contributors">Contributors</HashLink> </li>
+          <li>
+            {' '}
+            <HashLink to="/#!">Home</HashLink>{' '}
+          </li>
+          <li>
+            {' '}
+            <HashLink to="/#what-we-do">What we Do?</HashLink>{' '}
+          </li>
+          <li>
+            {' '}
+            <HashLink to="/interface">Design System</HashLink>{' '}
+          </li>
+          <li>
+            {' '}
+            <HashLink to="/#contributors">Contributors</HashLink>{' '}
+          </li>
         </S.FooterLinks>
       </S.FooterSections>
     </S.Container>
 
     <S.Socials>
       <S.FooterSocial>
-        <li> <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/Ignitus.org/"> <i className="fa fa-facebook"/></a> </li>
-        <li> <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/ignitus_org/"> <i className="fa fa-twitter"/></a> </li>
-        <li> <a target="_blank" rel="noopener noreferrer" href="https://angel.co/ignitus-2"> <i className="fa fa-angellist"/></a> </li>
-        <li> <a target="_blank" rel="noopener noreferrer" href="https://bit.ly/JoinIgnitusSlack"> <i className="fa fa-slack"/></a> </li>
-        <li> <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/company/13733583/admin/updates/"> <i className="fa fa-linkedin"/></a> </li>
-        <li> <a target="_blank" rel="noopener noreferrer" href="https://github.com/Ignitus"> <i className="fa fa-github"/></a> </li>
-        <li> <a target="_blank" rel="noopener noreferrer" href="https://medium.com/@social_63397"> <i className="fa fa-medium"/></a> </li>
-        <li> <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UCW2BmTcaghAmhELP4_RdRrw"> <i className="fa fa-youtube"/></a> </li>
+        <li>
+          {' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/Ignitus.org/"
+          >
+            {' '}
+            <S.Icon name={AppIcon.FacebookIcon} />
+          </a>{' '}
+        </li>
+        <li>
+          {' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/ignitus_org/"
+          >
+            {' '}
+            <S.Icon name={AppIcon.TwitterIcon} />
+          </a>{' '}
+        </li>
+        <li>
+          {' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://angel.co/ignitus-2"
+          >
+            {' '}
+            <S.Icon name={AppIcon.AngelListIcon} />
+          </a>{' '}
+        </li>
+        <li>
+          {' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://bit.ly/JoinIgnitusSlack"
+          >
+            {' '}
+            <i className="fa fa-slack" />
+          </a>{' '}
+        </li>
+        <li>
+          {' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/company/13733583/admin/updates/"
+          >
+            {' '}
+            <S.Icon name={AppIcon.LinkedInIcon} />
+          </a>{' '}
+        </li>
+        <li>
+          {' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/Ignitus"
+          >
+            {' '}
+            <i className="fa fa-github" />
+          </a>{' '}
+        </li>
+        <li>
+          {' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://medium.com/@social_63397"
+          >
+            {' '}
+            <i className="fa fa-medium" />
+          </a>{' '}
+        </li>
+        <li>
+          {' '}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.youtube.com/channel/UCW2BmTcaghAmhELP4_RdRrw"
+          >
+            {' '}
+            <i className="fa fa-youtube" />
+          </a>{' '}
+        </li>
       </S.FooterSocial>
     </S.Socials>
     <S.Copyright>&copy; 2018 Copyright Ignitus</S.Copyright>
   </S.Footer>
 );
 
-export const Footer = withErrorBoundary((() => {
+export const Footer = withErrorBoundary(() => {
   const [featuresToggle, setFeaturesToggle] = useState(true);
   const [communityToggle, setCommunityToggle] = useState(true);
   const [companyToggle, setCompanyToggle] = useState(true);
@@ -119,5 +274,4 @@ export const Footer = withErrorBoundary((() => {
       linkToggle={linkToggle}
     />
   );
-}));
-
+});
