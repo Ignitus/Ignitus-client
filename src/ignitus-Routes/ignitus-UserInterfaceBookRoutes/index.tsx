7d0c78c658bd7b8062f680d348f098dce2ffd276
Marvin9 <marvinduff97@gmail.com>
2020-04-20 02:13:21 +0530

    Add Progress component to UIBook

--- src/ignitus-Routes/ignitus-UserInterfaceBookRoutes/index.tsx
+++ src/ignitus-Routes/ignitus-UserInterfaceBookRoutes/index.tsx
@@ -26,6 +26,7 @@ import {interfaceOverlay} from '../../ignitus-UserInterfaceBook/Components/Molec
 import {interfaceFilters} from '../../ignitus-UserInterfaceBook/Components/Templates/interfaceFilters/Components/index';
 import {interfaceUserProfile} from '../../ignitus-UserInterfaceBook/Components/Organisms/interfaceUserProfile/Components';
 import {InterfaceAvatar} from '../../ignitus-UserInterfaceBook/Components/Organisms/interfaceAvatar/Components';
+import {InterfaceProgress} from '../../ignitus-UserInterfaceBook/Components/Molecules/interfaceProgress/index';
 
 const Container = styled.div`
   display: flex;
@@ -117,6 +118,7 @@ const UserInterfaceBookRoutes: React.FunctionComponent = () => (
         <Route path="/interface/userProfile" component={interfaceUserProfile} />
         <Route path="/interface/overlay" component={interfaceOverlay} />
         <Route path="/interface/avatar" component={InterfaceAvatar} />
+        <Route path="/interface/progress" component={InterfaceProgress} />
       </Switch>
     </LeftRow>
   </Container>
---
+++ src/ignitus-UserInterfaceBook/Components/Molecules/interfaceProgress/index.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+
+import { Interface } from '../../../styles';
+import { Heading2 } from '../../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/typography';
+import { Button } from '../../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/buttons';
+import { Progress } from '../../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Molecules/ignitus-Progress';
+import { flexibleRowDiv } from '../../../../ignitus-Shared/ignitus-DesignSystem/shared';
+
+const Middle = styled(flexibleRowDiv)``;
+
+export const InterfaceProgress = () => {
+  const [current, updateCurrent] = useState(1);
+  const steps = 5;
+
+  const next = () => (current === steps + 1 ? null : updateCurrent(current + 1));
+  const previous = () => (current === 1 ? null : updateCurrent(current - 1));
+
+  return (
+    <Interface>
+      <Heading2>
+        Signup Progress
+      </Heading2>
+      <hr />
+      <br />
+      <Progress steps={steps} current={current} />
+      <br />
+      <Middle>
+        <Button category="primary" onClick={previous}>
+          Previous
+        </Button>
+        <Button category="primary" onClick={next}>
+          Next
+        </Button>
+      </Middle>
+    </Interface>
+  );
+};
