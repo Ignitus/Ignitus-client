468fe9c7294a8011b76da1871a94547ff44de708
Marvin9 <marvinduff97@gmail.com>
2020-04-20 16:32:58 +0530

    FAdded more props support & improve default style

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Molecules/ignitus-Progress/types.ts
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Molecules/ignitus-Progress/types.ts
@@ -1,10 +1,23 @@
-export type a = string;
+export interface ProgressProps {
+  steps?: number;
+  current?: number;
+  lineWidth?: number;
+  lineHeight?: number;
+  circle?: number;
+  lineTransitionDuration?: number;
+  circleTransitionDelayDuration?: number;
+};
 
 export interface CircleProps {
   fill?: boolean;
   current?: boolean;
+  size: number;
+  delay: number;
 };
 
 export interface LineProps {
   fill?: boolean;
+  width: number;
+  height: number;
+  transition: number;
 };
