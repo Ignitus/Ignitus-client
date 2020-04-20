088da03ae75653916c7ed60161c96bb81bd73022
Asfiya Shaikh <aysp.1999@gmail.com>
2020-04-21 02:53:37 +0530

    Added View Profile Button as a shared Component

---
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Molecules/ignitus-ViewProfileButton/Components/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from '../style';
+
+const ViewProfileButton = () => (
+  <S.Button>
+    <S.Text>View Profile</S.Text>
+  </S.Button>  
+)
+
+export default ViewProfileButton;
