5e852eade8124fb3444c069b19bda22f0b6a24fb
Meghana <meghana@iitbhilai.ac.in>
2020-04-23 02:37:49 +0530

    added filter functionality

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/ignitus-secondaryDropDown/Components/secondaryDropDown.jsx
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/ignitus-secondaryDropDown/Components/secondaryDropDown.jsx
@@ -5,33 +5,35 @@ import {Details} from '../constants';
 
 
 export default function SecondaryDropDown() {
-  const [showOptions, setShowOptions] = useState(false);
+  const [searchTerm, setSearchTerm] = React.useState(" ");
+  const [searchResults, setSearchResults] = React.useState([]);
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+  React.useEffect(() => {
+    const results = Details.filter(person =>
+      person.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+
   return (
     <S.ParentContainer className="ParentContainer">
-      <S.SearchBar onChange={() => setShowOptions(1)} />
-      {showOptions && (
-        <Cards />
-      )}
+      <S.SearchBar onChange={handleChange} value={searchTerm}/>
+      <S.OptionsContainer>
+        {searchResults.map((person, index) =>
+          <S.CardWrapper key={person.name + "_" + index}>
+            <S.Avatar
+              src={person.avatarLink}
+            />
+            <S.NameWrapper>
+              <H.Heading5>
+                {person.name}
+              </H.Heading5>
+            </S.NameWrapper>
+          </S.CardWrapper>
+        )}
+      </S.OptionsContainer>
     </S.ParentContainer>
   );
-}
-
-
-const Cards = () => {
-  return (
-    <S.OptionsContainer>
-      {Details.map((person, index) =>
-        <S.CardWrapper key={person.name + "_" + index}>
-          <S.Avatar
-            src={person.avatarLink}
-          />
-          <S.NameWrapper>
-            <H.Heading5>
-              {person.name}
-            </H.Heading5>
-          </S.NameWrapper>
-        </S.CardWrapper>
-      )}
-    </S.OptionsContainer>
-  )
 }
