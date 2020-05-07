cb457a59b703be4d8fddc2c420d148ae0905fd46
Dishebh <dishebh27@gmail.com>
2020-05-07 15:00:29 +0530

    updated test

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Organisms/ignitus-DashboardFooter/Components/index.test.tsx
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Organisms/ignitus-DashboardFooter/Components/index.test.tsx
@@ -18,8 +18,13 @@ describe('<DashboardFooter />', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should render li, p tags', () => {
+  it('should render ul, li, p tags', () => {
+    expect(wrapper.find('ul').length).toBe(2);
     expect(wrapper.find('li').length).toBe(5);
     expect(wrapper.find('p').length).toBe(1);
+  });
+
+  it('should render 2018 copywright text', () => {
+    expect(wrapper.find('p').text()).toEqual('© Ignitus 2018');
   });
 });
