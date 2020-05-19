8dd6f7542fc19197a605ae3efd4921ca723bc428
Dishebh <dishebh27@gmail.com>
2020-05-19 14:27:08 +0530

    removed tests except snapshot matching

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Layout/ignitus-NotFound/Components/index.test.tsx
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Layout/ignitus-NotFound/Components/index.test.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import { mount, render } from 'enzyme';
+import { render } from 'enzyme';
 import emotionSerializer from 'jest-emotion';
 import { Notfound } from './index';
-import * as S from '../style';
-
-let wrapper;
 
 expect.addSnapshotSerializer(emotionSerializer);
 
 describe('<Notfound />', () => {
-  beforeEach(() => {
-    wrapper = mount(<Notfound />);
-  });
-
   it('should render', () => {
-    wrapper = render(<Notfound />);
+    const wrapper = render(<Notfound />);
     expect(wrapper).toMatchSnapshot();
-  });
-
-  it('should render Eyes, Message, Button, AnimationText components', () => {
-    expect(wrapper.find(S.Eyes).length).toBe(2);
-    expect(wrapper.find(S.Message).length).toBe(1);
-    expect(wrapper.find(S.StyledButton).length).toBe(1);
-    expect(wrapper.find(S.AnimationText).length).toBe(1);
   });
 });
