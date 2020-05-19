8b365ce7172d5527a5029de1fb2c8f2f57b12432
Dishebh <dishebh27@gmail.com>
2020-05-19 15:06:48 +0530

    added round borders

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Molecules/ignitus-ScrollBar/styles.ts
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Molecules/ignitus-ScrollBar/styles.ts
@@ -10,27 +10,19 @@ export const Scroll = styled.div`
   margin-bottom: 25px;
 
   ::-webkit-scrollbar {
-    width: 10px;
+    width: 12px;
     background: #f5f5f5;
   }
 
   ::-webkit-scrollbar-track {
     -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
-    background: #f5f5f5;
+    border-radius: 10px;
+    background-color: #f5f5f5;
   }
 
   ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
     background-color: #f90;
-    background-image: -webkit-linear-gradient(
-      90deg,
-      rgba(255, 255, 255, 0.2) 25%,
-      transparent 25%,
-      transparent 50%,
-      rgba(255, 255, 255, 0.2) 50%,
-      rgba(255, 255, 255, 0.2) 75%,
-      transparent 75%,
-      transparent
-    );
   }
 `;
 
