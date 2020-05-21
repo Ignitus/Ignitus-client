1081b2b4d5ed1be07bba9fba7ad0e0b2b370583b
Rawat <divyanshu.r46956@gmail.com>
2020-05-21 14:18:38 +0200

    Minor Updates 🎯

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Molecules/ignitus-ScrollBar/styles.ts
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Molecules/ignitus-ScrollBar/styles.ts
@@ -15,7 +15,7 @@ export const Scroll = styled.div`
   }
 
   ::-webkit-scrollbar-track {
-    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
     border-radius: 10px;
     background-color: #f5f5f5;
   }
