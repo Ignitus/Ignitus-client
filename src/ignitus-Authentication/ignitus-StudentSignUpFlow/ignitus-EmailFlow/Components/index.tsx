d8535c0f2e01fff239510865e596ce50b35d2a57
Dishebh Bhayana <dishebh27@gmail.com>
2020-06-29 23:24:22 +0530

    replaced signup progress image with component

--- src/ignitus-Authentication/ignitus-StudentSignUpFlow/ignitus-EmailFlow/Components/index.tsx
+++ src/ignitus-Authentication/ignitus-StudentSignUpFlow/ignitus-EmailFlow/Components/index.tsx
@@ -2,13 +2,11 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { FunctionComponent } from 'react';
-import { Link } from 'react-router-dom';
 import newsletter from '../../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Assets/ignitus-Images/img-Png/newsletterIcon.png';
 import {
   Paragraph,
   Heading2,
   withErrorBoundary,
-  Button,
 } from '../../../../ignitus-Shared';
 import * as T from '../../ignitus-WelcomeFlow/Styles/style';
 import * as C from '../Styles/style';
@@ -16,10 +14,6 @@ import * as C from '../Styles/style';
 export const EmailFlow: FunctionComponent = withErrorBoundary(() => (
   <T.WelcomeContainer>
     <T.TopSection>
-      <T.Progress
-        src="https://storage.googleapis.com/ignitus_assets/ig-assets/progressFive.png"
-        alt="progress-bar"
-      />
       <C.NewsletterImage src={newsletter} alt="newsletter" />
       <Heading2>Let’s confirm your email.</Heading2>
       <Paragraph>
@@ -31,18 +25,5 @@ export const EmailFlow: FunctionComponent = withErrorBoundary(() => (
         Click the link we sent to sofia.carter@gmail.com to confirm you email.
       </Paragraph>
     </T.TopSection>
-    <T.BottomSection>
-      <C.ButtonContainer>
-        <Button size="large" category="primary">
-          <Link to="/flow/emailConfirmationFlow">Email confirmed</Link>
-        </Button>
-        <Button size="large" category="white">
-          <Link to="/">I don’t see the email</Link>
-        </Button>
-      </C.ButtonContainer>
-      <Paragraph>
-        <Link to="/">I don’t want to confirm my email yet.</Link>
-      </Paragraph>
-    </T.BottomSection>
   </T.WelcomeContainer>
 ));
