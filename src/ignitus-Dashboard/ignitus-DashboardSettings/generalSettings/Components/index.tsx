bd75f1b3459e045e086773c458bb947e50bef97a
Dishebh <dishebh27@gmail.com>
2020-07-04 22:01:40 +0530

    added generalSettings UI

--- src/ignitus-Dashboard/ignitus-DashboardSettings/generalSettings/Components/index.tsx
+++ src/ignitus-Dashboard/ignitus-DashboardSettings/generalSettings/Components/index.tsx
@@ -1,170 +1,79 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import * as P from '../styles';
-import {
-  AboutProps,
-  EducationProps,
-  HeadingProps,
-  RecommendationsProps,
-} from '../types';
-import {
-  AppIcon,
-  Black,
-  // ButtonBottomRight,
-  GreySecondaryText,
-  Paragraph,
-  RoundedButton,
-} from '../../../../ignitus-Shared';
-// import { DefaultInput } from '../../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/ignitus-defaultInput/Components';
+import { Paragraph, Heading5 } from '../../../../ignitus-Shared';
+import { White } from '../../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/colors';
+import { UserSettingsSideNavigation } from '../../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Organisms/ignitus-SideNavigation/ignitus-UserSettingsSideNavigation/Components';
 
 export const StudentProfile = () => (
   <P.ParentContainer>
-    <P.TopSection>
-      <P.InputSection>
-        <P.Text>Username</P.Text>
-        <P.Input placeholder="Username" type="text" name="username" />
-      </P.InputSection>
-      <P.InputSection>
-        <P.Text>Name</P.Text>
-        <P.Input placeholder="Name" type="text" name="name" />
-      </P.InputSection>
-      <P.InputSection>
-        <P.Text>Email</P.Text>
-        <P.Input placeholder="Email" type="text" name="email" />
-      </P.InputSection>
-    </P.TopSection>
-    <P.MiddleSection>
-      <P.ElementContainer>
-        <About content="Well-versed in over a dozen literary genres and can teach to any range of students. Adept at creating a lesson plan that engages students helping students to see the beauty in literature and encouraging students to read on their own. Specializes in high school and junior college level classes." />
-      </P.ElementContainer>
-
-      <P.ElementContainer>
-        <Education
-          university="Graham Junior College"
-          address="New Parkland,CA"
-          batch="2015 – 2019"
-        />
-      </P.ElementContainer>
-
-      <P.ElementContainer>
-        <Heading title="Research Fields" icon={AppIcon.LocalLibraryIcon} />
-        <P.Content>
-          <RoundedButton size="medium" category="grey">
-            Psycholinguistics{' '}
-          </RoundedButton>
-          <RoundedButton size="medium" category="grey">
-            Modernist Literature{' '}
-          </RoundedButton>
-        </P.Content>
-      </P.ElementContainer>
-
-      <P.ElementContainer>
-        <Heading title="Publications" icon={AppIcon.LibraryBooksIcon} />
-        <P.Content>
-          <Heading
-            title="Shakespeare and Elizabethan Poetry: A Study of His Earlier Work in Relation to the Poetry of the Time."
-            icon={AppIcon.PdfIcon}
-            fontStyle="paragraph"
-          />
-          <Heading
-            title="Practice in a second language: Perspectives from applied linguistics and cognitive psychology."
-            icon={AppIcon.PdfIcon}
-            fontStyle="paragraph"
-          />
-        </P.Content>
-      </P.ElementContainer>
-
-      <P.ElementContainer>
-        <Heading title="Contributions" icon={AppIcon.ContributionIcon} />
-        <P.Content>
-          <P.ContributionsWrapper>
-            <Heading
-              title="5 Articles"
-              icon={AppIcon.LibraryBooksIcon}
-              fontStyle="paragraph"
+    <P.LeftContainer>
+      <UserSettingsSideNavigation />
+    </P.LeftContainer>
+    <P.RightContainer>
+      <P.TopSection>
+        <P.InputSection>
+          <P.Text>Username</P.Text>
+          <P.Input placeholder="Username" type="text" name="username" />
+        </P.InputSection>
+        <P.InputSection>
+          <P.Text>Name</P.Text>
+          <P.Input placeholder="Name" type="text" name="name" />
+        </P.InputSection>
+        <P.InputSection>
+          <P.Text>Email</P.Text>
+          <P.Input placeholder="Email" type="text" name="email" />
+        </P.InputSection>
+        <P.ImageSection>
+          <P.Text>Photo</P.Text>
+          <P.ImageContainer>
+            <P.Image
+              alt="Student"
+              src="https://storage.googleapis.com/ignitus_assets/ig-avatars/grant.png"
             />
-            <Heading
-              title="5 Polls"
-              icon={AppIcon.PollIcon}
-              fontStyle="paragraph"
+            <P.StyledRoundedButton size="large" category="grey">
+              Change
+            </P.StyledRoundedButton>
+          </P.ImageContainer>
+        </P.ImageSection>
+      </P.TopSection>
+      <P.MiddleSection>
+        <P.ElementContainer>
+          <P.StyledParagraph>
+            To save your changes, please enter your password.
+          </P.StyledParagraph>
+          <P.InputSection>
+            <P.Text>Current Password</P.Text>
+            <P.Input
+              placeholder="Current password"
+              type="password"
+              name="password"
             />
-            <Heading
-              title="2 Publications"
-              icon={AppIcon.PdfIcon}
-              fontStyle="paragraph"
-            />
-            <Heading
-              title="3 Videos"
-              icon={AppIcon.VideoLibIcon}
-              fontStyle="paragraph"
-            />
-          </P.ContributionsWrapper>
-        </P.Content>
-      </P.ElementContainer>
-
-      <RecommendationsSection
-        name="Nicholas Young"
-        designation="Professor"
-        date="December 19, 2018"
-        experience="I worked with Sophia in a research paper, for 2 years. She has the ability to understand toughest things effortlessly that skill often takes time to develop, but it seemed to come perfectly naturally to her. It's my privilege to recommend her even I found myself lucky that I got a chance to be in touch with Sophia."
-      />
-    </P.MiddleSection>
-    <P.BottomSection>
-      <RoundedButton size="medium" category="grey">
-        Download as PDF
-      </RoundedButton>
-    </P.BottomSection>
+          </P.InputSection>
+          <P.RightSection>
+            <P.ParagraphLink to="#!">Forgot your password?</P.ParagraphLink>
+          </P.RightSection>
+          <P.RightSection>
+            <P.StyledRoundedButton
+              size="medium"
+              category="orange"
+              color={White}
+            >
+              Save Changes
+            </P.StyledRoundedButton>
+          </P.RightSection>
+        </P.ElementContainer>
+      </P.MiddleSection>
+      <P.BottomSection>
+        <Heading5>Delete my account</Heading5>
+        <Paragraph>
+          Permanently delete your Ignitus account and all data associated with
+          it. If you experienced an issue with your account and need help,
+          please contact us so we can assist you.
+        </Paragraph>
+        <P.StyledRoundedButton size="medium" category="primary">
+          Delete Account
+        </P.StyledRoundedButton>
+      </P.BottomSection>
+    </P.RightContainer>
   </P.ParentContainer>
-);
-
-const Heading = ({ title, icon, fontStyle }: HeadingProps) => (
-  <P.TitleWrapper>
-    <P.Icon name={icon} />
-    <P.Title fontStyle={fontStyle}>{title}</P.Title>
-  </P.TitleWrapper>
-);
-
-const About = ({ content }: AboutProps) => (
-  <Fragment>
-    <Heading title="About" icon={AppIcon.InfoIcon} />
-    <P.Content>
-      <Paragraph color={GreySecondaryText}>{content}</Paragraph>
-    </P.Content>
-  </Fragment>
-);
-
-const Education = ({ university, address, batch }: EducationProps) => (
-  <Fragment>
-    <Heading title="Education" icon={AppIcon.GraduationCapIcon} />
-    <P.Content>
-      <P.University color={Black}>{university}</P.University>
-      <P.Address>{address}</P.Address>
-      <P.Date>{batch}</P.Date>
-    </P.Content>
-  </Fragment>
-);
-
-const RecommendationsSection = ({
-  name,
-  designation,
-  date,
-  experience,
-}: RecommendationsProps) => (
-  <P.ElementContainer>
-    <Heading title="Recommendations" icon={AppIcon.StarCircleIcon} />
-    <P.Content>
-      <P.RecommendationsRow>
-        <P.LeftRow>
-          <P.Avatar src="https://storage.googleapis.com/ignitus_assets/ig-avatars/eugene.png" />
-          <div>
-            <P.Name>{name}</P.Name>
-            <P.Designation>{designation}</P.Designation>
-            <P.Date>{date}</P.Date>
-          </div>
-        </P.LeftRow>
-        <P.RightRow>
-          <Paragraph color={GreySecondaryText}>{experience}</Paragraph>
-        </P.RightRow>
-      </P.RecommendationsRow>
-    </P.Content>
-  </P.ElementContainer>
 );
--- src/ignitus-Dashboard/ignitus-DashboardSettings/generalSettings/styles.ts
+++ src/ignitus-Dashboard/ignitus-DashboardSettings/generalSettings/styles.ts
@@ -1,26 +1,35 @@
 import styled from '@emotion/styled';
-import {
-  Container,
-  flexibleRowDiv,
-} from '../../../ignitus-Shared/ignitus-DesignSystem/shared';
+import { Link } from 'react-router-dom';
+import { Container } from '../../../ignitus-Shared/ignitus-DesignSystem/shared';
 
 import {
-  Heading5,
-  Paragraph,
-  Heading6,
   minimumWidthQuery,
   DefaultInput,
+  Paragraph,
+  RoundedButton,
 } from '../../../ignitus-Shared';
-import { TitleProps } from './types';
-import { Icon as I } from '../../../ignitus-Shared/ignitus-Utilities/Components/icon';
-import * as C from '../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/colors';
-import * as F from '../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/fonts';
+import {
+  White,
+  Orange,
+  IgnitusBlue,
+} from '../../../ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/colors';
 
-export const ParentContainer = styled(Container)`
+export const ParentContainer = styled.div`
+  display: flex;
+`;
+
+export const LeftContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  margin: 10rem 1rem 0 2rem;
+`;
+
+export const RightContainer = styled(Container)`
   margin: 10rem auto;
-  background: ${C.White};
+  background: ${White};
   box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.1);
   border-radius: 2rem;
+  max-width: 30rem;
 `;
 
 export const TopSection = styled.div`
@@ -34,45 +43,7 @@ export const ElementContainer = styled(Container)`
   margin: 1.5rem 0;
 `;
 
-export const Title = styled(Heading5)<TitleProps>`
-  padding: 0.5rem 2rem;
-  font-weight: ${props =>
-    props.fontStyle === 'paragraph' ? F.Medium : F.Bold};
-  font-size: ${props => (props.fontStyle === 'paragraph' ? F.SM : F.MD)};
-  color: ${props =>
-    props.fontStyle === 'paragraph' ? C.GreyText : C.IgnitusBlue};
-`;
-
-export const TitleWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-export const Icon = styled(I)`
-  width: 1.5rem;
-  fill: ${C.IgnitusBlue};
-`;
-
-export const Content = styled(Container)`
-  margin-left: 3.6rem;
-  margin-top: 1rem;
-`;
-
-export const University = styled(Heading5)`
-  font-weight: ${F.SemiBold};
-`;
-
-export const Address = styled(Paragraph)`
-  font-size: ${F.MD};
-  color: ${C.GreySecondaryText};
-  margin: 0.4rem 0;
-`;
-
-export const Date = styled(Paragraph)`
-  margin: 0.4rem 0;
-`;
-
-export const ContributionsWrapper = styled.div`
+export const ImageContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -95,41 +66,16 @@ export const ContributionsWrapper = styled.div`
   }
 `;
 
-export const RecommendationsRow = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-`;
-
-export const LeftRow = styled(flexibleRowDiv)`
-  flex-wrap: wrap;
-  align-items: space-evenly;
-  flex: 1;
-`;
-
-export const RightRow = styled(flexibleRowDiv)`
-  flex: 2;
-  margin-left: 2rem;
-`;
-export const Avatar = styled.img`
-  margin: 1rem;
-  width: 9rem;
-  border-radius: 50%;
-`;
-export const Name = styled(Heading5)``;
-export const Designation = styled(Heading6)`
-  margin: 0.4rem 0;
-`;
-
 export const BottomSection = styled.div`
-  text-align: center;
-  margin: 1rem;
+  text-align: left;
+  margin-top: -3rem;
   padding: 2rem;
 `;
 
 export const Text = styled(Paragraph)`
   font-size: 1.5rem;
   margin: 0.5rem 0.4rem 0 2rem;
+  min-width: 10rem;
 `;
 
 export const Input = styled(DefaultInput)`
@@ -138,6 +84,46 @@ export const Input = styled(DefaultInput)`
 
 export const InputSection = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
   margin: 1rem;
+`;
+
+export const ImageSection = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  margin: 1rem;
+`;
+
+export const Image = styled.img`
+  height: 10rem;
+  width: 10rem;
+  border-radius: 0.9rem;
+`;
+
+export const StyledParagraph = styled(Paragraph)`
+  font-weight: bold;
+`;
+
+export const StyledRoundedButton = styled(RoundedButton)`
+  display: flex;
+  font-weight: bold;
+  color: ${props => props.color};
+  margin-left: 1rem;
+  height: 3rem;
+`;
+
+export const RightSection = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const ParagraphLink = styled(Link)`
+  display: block;
+  font-size: 0.8rem;
+  color: ${Orange};
+  &:hover {
+    color: ${IgnitusBlue};
+  }
 `;
