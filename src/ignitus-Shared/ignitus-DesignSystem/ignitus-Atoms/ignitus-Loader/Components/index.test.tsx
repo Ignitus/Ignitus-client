259d6f240e10be9d28b45494608d956308b1b288
upieez <samuelhww@gmail.com>
2020-08-20 22:36:19 +0800

    add extra test cases

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/ignitus-Loader/Components/index.test.tsx
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/ignitus-Loader/Components/index.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { mount, render } from 'enzyme';
 
 import { Loader } from './index';
+import * as I from '../styles';
 
 it('should render', () => {
   const tree = render(<Loader />);
   expect(tree).toMatchSnapshot();
+});
+
+describe('<Loader />', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<Loader />);
+  });
+
+  it('should render Spinner once', () => {
+    expect(wrapper.find(I.Spinner).length).toBe(1);
+  });
+
+  it('should render Logo once', () => {
+    expect(wrapper.find(I.Logo).length).toBe(1);
+  });
 });
