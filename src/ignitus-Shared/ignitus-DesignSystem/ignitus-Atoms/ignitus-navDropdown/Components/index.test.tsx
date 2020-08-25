505be12b0eac539da961edef7a3fe7e3c2270c5c
upieez <samuelhww@gmail.com>
2020-08-25 22:48:29 +0800

    add test for Dashboard Navigation Dropdown

---
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/ignitus-navDropdown/Components/index.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { mount, render } from 'enzyme';
+import { DashboardNavigationDropdown } from './index';
+
+import * as S from '../styles';
+import { AppIcon } from '../../../../types/iconsTypes/iconEnums';
+
+const props = {
+  Options: [{ title: 'title' }],
+  Name: 'name',
+};
+
+it('should render', () => {
+  const tree = render(<DashboardNavigationDropdown {...props} />);
+  expect(tree).toMatchSnapshot();
+});
+
+describe('<DashboardNavigationDropdown />', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(<DashboardNavigationDropdown {...props} />);
+  });
+
+  it('allows us to set Name props', () => {
+    expect(wrapper.props().Name).toEqual('name');
+    wrapper.setProps({ Name: 'different name' });
+    expect(wrapper.props().Name).toEqual('different name');
+  });
+
+  it('allows us to set Options props', () => {
+    expect(wrapper.props().Options).toEqual([{ title: 'title' }]);
+    wrapper.setProps({
+      Options: [{ title: 'second title' }, { title: 'third title' }],
+    });
+    expect(wrapper.props().Options).toEqual([
+      { title: 'second title' },
+      { title: 'third title' },
+    ]);
+  });
+
+  it('should render given icon', () => {
+    expect(wrapper.find(S.Icon).length).toBe(2);
+    expect(wrapper.find(AppIcon.StudentIcon).length).toBe(1);
+    expect(wrapper.find(AppIcon.FilledArrowDownIcon).length).toBe(1);
+  });
+});
