bd3fc1f843058493e498afeab92b123c81249c80
upieez <samuelhww@gmail.com>
2020-08-27 01:23:11 +0800

    refactor changes from review
    however, the test that I wrote I am unsure and would need more clarification. I wrote two other tests regarding state but they are failing. simulating click event does not seem to work

--- src/ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/ignitus-navDropdown/Components/index.test.tsx
+++ src/ignitus-Shared/ignitus-DesignSystem/ignitus-Atoms/ignitus-navDropdown/Components/index.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { mount, render } from 'enzyme';
 import { DashboardNavigationDropdown } from './index';
 
-import { Icon } from '../styles';
+import { Icon, NavigationHeading } from '../styles';
 import { AppIcon } from '../../../../types/iconsTypes/iconEnums';
 
 const props = {
@@ -21,21 +21,21 @@ describe('<DashboardNavigationDropdown />', () => {
     wrapper = mount(<DashboardNavigationDropdown {...props} />);
   });
 
-  it('allows us to set Name props', () => {
-    expect(wrapper.props().Name).toEqual('name');
-    wrapper.setProps({ Name: 'different name' });
-    expect(wrapper.props().Name).toEqual('different name');
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  it('allows us to set Options props', () => {
-    expect(wrapper.props().Options).toEqual([{ title: 'title' }]);
-    wrapper.setProps({
-      Options: [{ title: 'second title' }, { title: 'third title' }],
-    });
-    expect(wrapper.props().Options).toEqual([
-      { title: 'second title' },
-      { title: 'third title' },
-    ]);
+  it('it should render Name prop inside NavigationHeading surrounded by static icons', () => {
+    expect(
+      wrapper
+        .find(NavigationHeading)
+        .children()
+        .find(Icon).length,
+    ).toBe(2);
+    expect(wrapper.find(AppIcon.StudentIcon).length).toBe(1);
+    expect(wrapper.find(AppIcon.FilledArrowDownIcon).length).toBe(1);
+    expect(wrapper.props().Name).toBe(props.Name); // ? unsure on how to write this part
+    // expect(wrapper.find(NavigationHeading).children().text()).toBe(props.Name); // ? initially wrote it like this but can't figure out how to trim away the icon text name
   });
 
   it('should render given icon', () => {
@@ -43,4 +43,23 @@ describe('<DashboardNavigationDropdown />', () => {
     expect(wrapper.find(AppIcon.StudentIcon).length).toBe(1);
     expect(wrapper.find(AppIcon.FilledArrowDownIcon).length).toBe(1);
   });
+
+  // ! both tests below are failing
+
+  // it('should toggle state on click', () => {
+  //   const setContent = jest.fn();
+  //   const handleClick = jest.spyOn(React, 'useState');
+  //   handleClick.mockImplementation(content => [content, setContent]);
+
+  //   wrapper.find(NavigationHeading).simulate('click');
+  //   expect(setContent).toHaveBeenCalledTimes(1);
+  //   wrapper.find(NavigationHeading).simulate('click');
+  //   expect(setContent).toHaveBeenCalledTimes(2);
+  // });
+
+  // it('it should render Options only after simulating click', () => {
+  //   expect(wrapper.find(Column).length).toBe(0);
+  //   wrapper.find(NavigationHeading).simulate('click');
+  //   console.log(wrapper.debug());
+  // });
 });
